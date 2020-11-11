3d576ba981ec3427121079256e8508b335a1fd4e
Mark Romphf <markromphf@gmail.com>
2020-11-11 15:56:05 -0600

    Render a card image to start.

---
+++ src/blackjack/Controller.java
@@ -0,0 +1,32 @@
+package blackjack;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    private Canvas foreground;
+
+    private final Image img = new Image("file:graphics/clubs02.jpg");
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        Rectangle2D screen = javafx.stage.Screen.getPrimary().getBounds();
+        GraphicsContext context = foreground.getGraphicsContext2D();
+        double screenHeight = screen.getHeight();
+        double screenWidth = screen.getWidth();
+
+        foreground.setHeight(screenHeight);
+        foreground.setWidth(screenWidth);
+        context.drawImage(img, 300, 300);
+    }
+}
---
+++ src/blackjack/Main.java
@@ -0,0 +1,26 @@
+package blackjack;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("ui/blackjack.fxml"));
+        Scene scene = new Scene(root);
+
+        root.requestFocus();
+        primaryStage.setTitle("Blackjack");
+        primaryStage.setScene(scene);
+        primaryStage.setFullScreen(true);
+        primaryStage.setMaximized(true);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}

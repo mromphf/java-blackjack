ebec6a08b14b0cc9fb1a74c6b90081116903c782
Mark Romphf <markromphf@gmail.com>
2020-11-14 18:54:11 -0600

    Render a random image based on card pulled from deck.

--- src/blackjack/ui/Controller.java
+++ src/blackjack/ui/Controller.java
@@ -1,14 +1,16 @@
 package blackjack.ui;
 
+import blackjack.domain.Card;
+import blackjack.domain.Deck;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
-import javafx.scene.paint.Color;
 
 import java.net.URL;
+import java.util.Collection;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
@@ -16,17 +18,19 @@ public class Controller implements Initializable {
     @FXML
     private Canvas foreground;
 
-    private final Image img = new Image("file:graphics/clubs02.jpg");
-
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         Rectangle2D screen = javafx.stage.Screen.getPrimary().getBounds();
         GraphicsContext context = foreground.getGraphicsContext2D();
         double screenHeight = screen.getHeight();
         double screenWidth = screen.getWidth();
+        Collection<Card> deck = Deck.shuffle();
+        Card c = deck.stream().iterator().next();
+        String imageName = c.getSuit().name().toLowerCase() + c.getValue();
+        final Image img = new Image(String.format("file:graphics/%s.jpg", imageName));
 
         foreground.setHeight(screenHeight);
         foreground.setWidth(screenWidth);
-        context.drawImage(img, 300, 300);
+        context.drawImage(img, 0, 0);
     }
 }

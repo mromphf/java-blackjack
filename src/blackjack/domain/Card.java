57bfa59757b1f6592b3ef3cd92b6d1b46e7c0f02
Mark Romphf <markromphf@gmail.com>
2020-11-15 14:53:27 -0600

    Override equals() and hashCode() in Card class.

--- src/blackjack/domain/Card.java
+++ src/blackjack/domain/Card.java
@@ -1,5 +1,7 @@
 package blackjack.domain;
 
+import java.util.Objects;
+
 public class Card {
     private final int value;
     private final Suit suit;
@@ -19,5 +21,19 @@ public class Card {
 
     public Suit getSuit() {
         return suit;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Card card = (Card) o;
+        return value == card.value &&
+                suit == card.suit;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value, suit);
     }
 }

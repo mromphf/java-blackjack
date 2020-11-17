83ad7a657c537cc708995596d63a9c92fdbc339b
Mark Romphf <markromphf@gmail.com>
2020-11-16 18:44:10 -0600

    Separate opening hand dealing and image map assembly.

--- src/blackjack/ui/Controller.java
+++ src/blackjack/ui/Controller.java
@@ -1,4 +1,5 @@
 package blackjack.ui;
+import blackjack.domain.Card;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
@@ -7,6 +8,11 @@ import javafx.scene.image.Image;
 import java.net.URL;
 import java.util.*;
 
+import static blackjack.domain.Deck.fresh;
+import static blackjack.domain.Deck.shuffle;
+import static blackjack.ui.IMAGE_KEY.*;
+import static blackjack.ui.IMAGE_KEY.PLR_CARD_2;
+
 public class Controller implements Initializable {
 
     @FXML
@@ -14,8 +20,28 @@ public class Controller implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        Stack<Card> deck = shuffle(fresh());
         Screen screen = new Screen(foreground);
-        Map<IMAGE_KEY, Image> imageMap = Round.opening();
+
+        Map<String, Set<Card>> hands = Round.opening(deck);
+        Iterator<Card> dealer = hands.get("dealer").iterator();
+        Iterator<Card> player = hands.get("player").iterator();
+
+        Map<IMAGE_KEY, Image> imageMap = imageMap( dealer, player );
         screen.drawCards(imageMap);
+    }
+
+    private Map<IMAGE_KEY, Image> imageMap(Iterator<Card> dealer, Iterator<Card> player) {
+        return new HashMap<IMAGE_KEY, Image>() {{
+            put(DLR_CARD_1, imageFile(dealer.next()));
+            put(DLR_CARD_2, imageFile(dealer.next()));
+            put(PLR_CARD_1, imageFile(player.next()));
+            put(PLR_CARD_2, imageFile(player.next()));
+        }};
+    }
+
+    private static Image imageFile(Card c) {
+        String imageName = c.getSuit().name().toLowerCase() + c.getValue();
+        return new Image(String.format("file:graphics/%s.jpg", imageName));
     }
 }

3d4a0820bfe922e146402abe1e546fb3772bc0e1
Mark Romphf <markromphf@gmail.com>
2020-11-19 19:20:52 -0600

    Improved GUI layout.

--- src/blackjack/ui/Screen.java
+++ src/blackjack/ui/Screen.java
@@ -4,24 +4,37 @@ import javafx.geometry.Rectangle2D;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 
 import java.util.Map;
 
 public class Screen {
 
+    private final static int CARD_WIDTH = 125;
+    private final static int CARD_HEIGHT = 150;
+    private final static int FOREGROUND_DIM = 800;
+
     private final GraphicsContext context;
 
     public Screen(Canvas foreground) {
-        context = foreground.getGraphicsContext2D();
         Rectangle2D screen = javafx.stage.Screen.getPrimary().getBounds();
-        foreground.setHeight(1000);
-        foreground.setWidth(screen.getWidth());
+        foreground.setHeight(FOREGROUND_DIM);
+        foreground.setWidth(FOREGROUND_DIM);
+
+        context = foreground.getGraphicsContext2D();
+
+        context.setFill(Color.WHITE);
+        context.fillRect(0, 0, foreground.getWidth(), foreground.getHeight());
+
+        context.setFill(Color.BLACK);
+        context.rect(0, 0, foreground.getWidth(), foreground.getHeight());
+        context.stroke();
     }
 
     public void drawCards(Map<IMAGE_KEY, Image> imageMap) {
-        context.drawImage(imageMap.get(IMAGE_KEY.DLR_CARD_1), 800, 200);
-        context.drawImage(imageMap.get(IMAGE_KEY.DLR_CARD_2), 1000, 200);
-        context.drawImage(imageMap.get(IMAGE_KEY.PLR_CARD_1), 800, 600);
-        context.drawImage(imageMap.get(IMAGE_KEY.PLR_CARD_2), 1000, 600);
+        context.drawImage(imageMap.get(IMAGE_KEY.DLR_CARD_1), 225, 100, CARD_WIDTH, CARD_HEIGHT);
+        context.drawImage(imageMap.get(IMAGE_KEY.DLR_CARD_2), 450, 100, CARD_WIDTH, CARD_HEIGHT);
+        context.drawImage(imageMap.get(IMAGE_KEY.PLR_CARD_1), 225, 400, CARD_WIDTH, CARD_HEIGHT);
+        context.drawImage(imageMap.get(IMAGE_KEY.PLR_CARD_2), 450, 400, CARD_WIDTH, CARD_HEIGHT);
     }
 }

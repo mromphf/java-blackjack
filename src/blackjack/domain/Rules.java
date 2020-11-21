d0c3d35ef2df30cb16b4e04bf447c26139952aa1
Mark Romphf <markromphf@gmail.com>
2020-11-21 14:20:25 -0600

    We don't need this options shit.

--- src/blackjack/domain/Rules.java
+++ src/blackjack/domain/Rules.java
@@ -1,8 +1,6 @@
 package blackjack.domain;
 
 import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
 
 public class Rules {
     public static boolean isBlackjack(Collection<Card> cards) {
@@ -10,23 +8,5 @@ public class Rules {
         boolean tenOrHigher = cards.stream().filter(c -> c.getValue() > 9).count() == 1;
         boolean twoCards = cards.size() == 2;
         return twoCards && oneAce && tenOrHigher;
-    }
-
-    public static Set<Option> options(Collection<Card> cards) {
-        int benchmark = cards.iterator().next().getValue();
-        if (cards.stream().allMatch(c -> c.getValue() == benchmark)) {
-            return new HashSet<Option>() {{
-                add(Option.DOUBLE);
-                add(Option.HIT);
-                add(Option.SPLIT);
-                add(Option.STAND);
-            }};
-        } else {
-            return new HashSet<Option>() {{
-                add(Option.DOUBLE);
-                add(Option.HIT);
-                add(Option.STAND);
-            }};
-        }
     }
 }

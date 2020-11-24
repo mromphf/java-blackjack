f19e29cdeba86bef77dc3017df1440787f26309c
Mark Romphf <markromphf@gmail.com>
2020-11-23 18:18:02 -0600

    View and controller for the Bet screen.

---
+++ src/blackjack/io/bet/BetController.java
@@ -0,0 +1,28 @@
+package blackjack.io.bet;
+
+import blackjack.Main;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class BetController implements Initializable {
+
+    @FXML
+    private Button btnDeal;
+
+    public BetController(Main main, FXMLLoader fxmlLoader) throws IOException {
+        fxmlLoader.setController(this);
+        fxmlLoader.load();
+        btnDeal.setOnAction(event -> main.switchToBlackjackScreen());
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}

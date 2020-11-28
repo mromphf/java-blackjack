369b5ada4465afb8c1828ec8cc3720cb731e5bcd
Mark Romphf <markromphf@gmail.com>
2020-11-28 16:36:08 -0600

    Hybrid event declaration in the BetView.

--- src/main/io/bet/BetController.java
+++ src/main/io/bet/BetController.java
@@ -6,7 +6,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.text.Font;
 import main.io.RootController;
 import main.usecase.ControlListener;
 import main.usecase.GameState;
@@ -43,13 +42,11 @@ public class BetController extends RootController implements Initializable, Game
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        lblBet.setFont(new Font("Arial", 50));
         btnBet1.setOnMouseClicked(event -> onBet(event, 1));
         btnBet5.setOnMouseClicked(event -> onBet(event, 5));
         btnBet10.setOnMouseClicked(event -> onBet(event, 10));
         btnBet25.setOnMouseClicked(event -> onBet(event, 25));
         btnBet100.setOnMouseClicked(event -> onBet(event, 100));
-        btnDeal.setOnAction(event -> onPlay());
     }
 
     @Override
@@ -57,6 +54,11 @@ public class BetController extends RootController implements Initializable, Game
         this.bet = gameState.bet;
         btnDeal.setDisable(bet <= 0);
         lblBet.setText("Bet: $" + bet);
+    }
+
+    @FXML
+    private void onDeal() {
+        controlListeners.forEach(ControlListener::onStartGame);
     }
 
     private void onBet(MouseEvent mouseEvent, int amount) {
@@ -69,9 +71,5 @@ public class BetController extends RootController implements Initializable, Game
         }
         btnDeal.setDisable(bet <= 0);
         lblBet.setText("Bet: $" + bet);
-    }
-
-    private void onPlay() {
-        controlListeners.forEach(ControlListener::onStartGame);
     }
 }

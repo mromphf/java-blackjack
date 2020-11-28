0c7f44ff9a4c6110eb2b1aca1a331713d068672a
Mark Romphf <markromphf@gmail.com>
2020-11-28 16:28:52 -0600

    Switch back to declarative event registration in Home Controller/View

--- src/main/io/home/HomeController.java
+++ src/main/io/home/HomeController.java
@@ -2,7 +2,6 @@ package main.io.home;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 
 import main.io.RootController;
 import main.usecase.ControlListener;
@@ -12,22 +11,15 @@ import java.util.ResourceBundle;
 
 public class HomeController extends RootController implements Initializable {
 
-    @FXML
-    public Button btnPlay;
-
-    @FXML
-    public Button btnExit;
-
     @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        btnPlay.setOnAction(event -> onPlay());
-        btnExit.setOnAction(event -> onExit());
-    }
+    public void initialize(URL location, ResourceBundle resources) {}
 
+    @FXML
     public void onPlay() {
         controlListeners.forEach(ControlListener::onMoveToBettingTable);
     }
 
+    @FXML
     public void onExit() {
         System.exit(0);
     }

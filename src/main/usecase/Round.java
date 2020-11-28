94264f3417aa25598858134935113d30f9ad4426
Mark Romphf <markromphf@gmail.com>
2020-11-28 09:35:15 -0600

    Create a class to represent one round of Blackjack.

---
+++ src/main/usecase/Round.java
@@ -0,0 +1,67 @@
+package main.usecase;
+
+import main.AppRoot;
+import main.Layout;
+import main.domain.Card;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
+
+import static main.domain.Rules.bust;
+import static main.domain.Rules.score;
+
+public class Round {
+
+    private final AppRoot appRoot;
+    private final Stack<Card> deck;
+    private final Map<String, List<Card>> hands;
+    private int bet = 0;
+
+    public Round(AppRoot appRoot, Stack<Card> deck, Map<String, List<Card>> hands) {
+        this.appRoot = appRoot;
+        this.deck = deck;
+        this.hands = hands;
+    }
+
+    public boolean playerBusted() {
+        return bust(hands.get("player"));
+    }
+
+    public int getBet() {
+        return bet;
+    }
+
+    public List<Card> getHand(String player) {
+        return hands.get(player);
+    }
+
+    public void start(int bet) {
+        this.bet = bet;
+        AppRoot.setLayout(Layout.GAME);
+    }
+
+    public void finishRound() {
+        this.bet = 0;
+        AppRoot.setLayout(Layout.BET);
+    }
+
+    public void hit() {
+        if (deck.isEmpty()) {
+            System.out.println("No more cards! Quitting...");
+            System.exit(0);
+        } else {
+            hands.get("player").add(deck.pop());
+        }
+    }
+
+    public void dealerTurn() {
+        while (score(hands.get("dealer")) < 16) {
+            if (deck.isEmpty()) {
+                System.out.println("No more cards! Quitting...");
+                System.exit(0);
+            }
+            hands.get("dealer").add(deck.pop());
+        }
+    }
+}

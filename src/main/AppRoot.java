bb359bf5d11543a06f4d6ceb42c9a6b376f0d673
Mark Romphf <markromphf@gmail.com>
2020-12-03 18:46:11 -0600

    stacked deck

--- src/main/AppRoot.java
+++ src/main/AppRoot.java
@@ -4,7 +4,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import main.domain.Card;
 import main.domain.Game;
+import main.domain.Suit;
 import main.io.bet.BetController;
 import main.io.blackjack.BlackjackController;
 import main.io.blackjack.ImageMap;
@@ -14,6 +16,7 @@ import main.usecase.Round;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Stack;
 
 import static main.domain.Deck.fresh;
 import static main.domain.Deck.shuffle;
@@ -39,7 +42,28 @@ public class AppRoot {
             e.printStackTrace();
         }
 
-        Game game = new Game(200, shuffle(fresh()));
+        Stack<Card> deck = new Stack<Card>() {{
+            add(new Card(13, Suit.CLUBS));
+            add(new Card(11, Suit.HEARTS));
+            add(new Card(10, Suit.HEARTS));
+            add(new Card(9, Suit.HEARTS));
+            add(new Card(6, Suit.CLUBS));
+            add(new Card(3, Suit.SPADES));
+            add(new Card(6, Suit.CLUBS));
+            add(new Card(5, Suit.HEARTS));
+            add(new Card(7, Suit.HEARTS));
+            add(new Card(9, Suit.DIAMONDS));
+            add(new Card(8, Suit.SPADES));
+            add(new Card(4, Suit.CLUBS));
+            add(new Card(1, Suit.HEARTS));
+            add(new Card(13, Suit.DIAMONDS));
+            add(new Card(11, Suit.CLUBS));
+            add(new Card(12, Suit.CLUBS));
+            add(new Card(13, Suit.CLUBS));
+            add(new Card(10, Suit.CLUBS));
+        }};
+        Game game = new Game(200, deck);
+        //Game game = new Game(200, shuffle(fresh()));
         Round round = new Round(game);
 
         HomeController homeController = homeLoader.getController();

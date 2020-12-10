befdcc1122b7688a92df7a41609f40da8a62807f
Mark Romphf <markromphf@gmail.com>
2020-12-10 17:04:34 -0600

    Nevermind.

--- src/main/io/blackjack/BlackjackController.java
+++ src/main/io/blackjack/BlackjackController.java
@@ -66,7 +66,6 @@ public class BlackjackController extends RootController implements Initializable
         lblBalance.setText(String.format("Balance: $%s", balance));
 
         if (snapshot.isResolved()) {
-            //TODO: Don't expose the table if the player busts
             renderExposedTable(snapshot);
 
             switch (snapshot.getOutcome()) {

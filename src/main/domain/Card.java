8f46e3e93cb0897b5fca01275c4754334372c9f2
Mark Romphf <markromphf@gmail.com>
2020-12-10 17:46:11 -0600

    Implement toString() on Card.

--- src/main/domain/Card.java
+++ src/main/domain/Card.java
@@ -1,5 +1,6 @@
 package main.domain;
 
+import java.util.HashMap;
 import java.util.Objects;
 import java.util.Random;
 
@@ -7,7 +8,7 @@ public class Card {
     private final int faceValue;
     private final Suit suit;
 
-    public Card(int value, Suit suit) {
+    public Card(int value, Suit suit) throws IllegalArgumentException {
         if (value > 13 || value < 1) {
             throw new IllegalArgumentException("Card value must be greater than 0 and less than 14.");
         }
@@ -31,6 +32,14 @@ public class Card {
         return suit;
     }
 
+    public String name() {
+        return new HashMap<Integer, String>() {{
+            put(11, "Jack");
+            put(12, "Queen");
+            put(13, "King");
+        }}.getOrDefault(faceValue, String.valueOf(faceValue));
+    }
+
     public static Card random() {
         Random r = new Random();
         int v = r.nextInt(13) + 1;
@@ -50,5 +59,10 @@ public class Card {
     @Override
     public int hashCode() {
         return Objects.hash(faceValue, suit);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s of %s", name(), suit);
     }
 }

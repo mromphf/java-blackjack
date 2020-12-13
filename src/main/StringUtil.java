85b370aafb3b4046bea66ad726bb807aee529d48
Mark Romphf <markromphf@gmail.com>
2020-12-13 13:22:30 -0600

    Create a StringUtil to implement some quality of life methods.

---
+++ src/main/StringUtil.java
@@ -0,0 +1,20 @@
+package main;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+public class StringUtil {
+    public static String concat(Collection<?> c, char delimiter) {
+        return chomp(c.stream()
+                .map(a -> String.format("%s%s ", a, delimiter))
+                .collect(Collectors.joining()), 2);
+    }
+
+    public static String chomp(String s, int n) {
+        if (s.length() > 0) {
+            return s.substring(0, s.length() - n);
+        } else {
+            return "";
+        }
+    }
+}

a48a2adacfe5daa5a14633f7dd1600c9d33b0a43
Mark Romphf <markromphf@gmail.com>
2020-12-17 18:38:40 -0600

    Read account info from CSV file.

--- src/main/io/storage/AccountStorage.java
+++ src/main/io/storage/AccountStorage.java
@@ -2,17 +2,11 @@ package main.io.storage;
 
 import main.domain.Account;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 public class AccountStorage {
 
     public static List<Account> getAll() {
-        return new ArrayList<Account>() {{
-            add(new Account(UUID.randomUUID(), "StickyJibs", 200));
-            add(new Account(UUID.randomUUID(), "Fast Browns", 100));
-            add(new Account(UUID.randomUUID(), "Sam Uncles", 450));
-        }};
+        return new SaveFile().getAllAccounts();
     }
 }
---
+++ src/main/io/storage/SaveFile.java
@@ -0,0 +1,34 @@
+package main.io.storage;
+
+import main.domain.Account;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URISyntaxException;
+import java.util.*;
+
+public class SaveFile {
+
+    public List<Account> getAllAccounts() {
+        List<Account> accounts = new ArrayList<>();
+
+        try {
+            Scanner sc = new Scanner(new File(SaveFile.class.getResource("/accounts.csv").toURI()));
+            sc.useDelimiter("\n");
+            sc.next();
+
+            while (sc.hasNext()) {
+                String[] line = sc.next().split(",");
+                accounts.add(new Account(UUID.fromString(line[0]), line[1], Integer.parseInt(line[2])));
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Could not load accounts file!");
+            return accounts;
+        } catch ( NoSuchElementException | URISyntaxException e) {
+            System.out.println(Arrays.toString(e.getStackTrace()));
+            System.exit(1);
+        }
+
+        return accounts;
+    }
+}

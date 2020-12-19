bde60cd655936279f527d09536e3d71156b38213
Mark Romphf <markromphf@gmail.com>
2020-12-19 13:20:20 -0600

    Add form for creating a new account.

--- src/main/io/home/HomeController.java
+++ src/main/io/home/HomeController.java
@@ -5,17 +5,32 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import main.domain.Account;
 import main.io.RootController;
 import main.usecase.TransactionListener;
 
 import java.net.URL;
+import java.time.LocalDateTime;
 import java.util.*;
 
 public class HomeController extends RootController implements Initializable, TransactionListener {
 
     @FXML
+    public GridPane listControls;
+
+    @FXML
+    public GridPane accountCreationControls;
+
+    @FXML
+    public TextField txtName;
+
+    @FXML
     private ListView<Account> lstAccounts;
+
+    @FXML
+    private Button btnOk;
 
     @FXML
     private Button btnPlay;
@@ -40,6 +55,40 @@ public class HomeController extends RootController implements Initializable, Tra
     public void onClickList() {
         final Account selectedAccount = lstAccounts.getSelectionModel().getSelectedItem();
         btnPlay.setDisable(selectedAccount == null);
+    }
+
+    @FXML
+    public void onKeyTyped() {
+        btnOk.setDisable(txtName.getText().length() < 3);
+    }
+
+    @FXML
+    public void onNew() {
+        btnOk.setDisable(txtName.getText().length() < 3);
+        accountCreationControls.setVisible(true);
+        listControls.setVisible(false);
+    }
+
+    @FXML
+    public void onCancel() {
+        accountCreationControls.setVisible(false);
+        listControls.setVisible(true);
+        txtName.setText("");
+    }
+
+    @FXML
+    public void onCreate() {
+        final UUID uuid = UUID.randomUUID();
+        final String name = txtName.getText();
+        final int balance = 200;
+        final LocalDateTime now = LocalDateTime.now();
+
+        accountMap.put(uuid, new Account(uuid, name, balance, now));
+
+        txtName.setText("");
+        accountCreationControls.setVisible(false);
+        listControls.setVisible(true);
+        lstAccounts.setItems(FXCollections.observableList(new ArrayList<>(accountMap.values())));
     }
 
     @Override

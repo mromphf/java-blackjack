44f5f105bec9fe145de2a3c0fa0a7730b20d00e1
Mark Romphf <markromphf@gmail.com>
2021-01-20 19:53:55 -0600

    Functioning date picker.

--- src/main/io/history/HistoryController.java
+++ src/main/io/history/HistoryController.java
@@ -1,11 +1,12 @@
 package main.io.history;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.Axis;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.DatePicker;
 import javafx.scene.layout.GridPane;
 import main.usecase.Layout;
 import main.domain.Account;
@@ -16,11 +17,13 @@ import main.usecase.NavListener;
 import main.usecase.TransactionListener;
 
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.ResourceBundle;
 
+import static main.domain.Transaction.*;
 import static main.usecase.Layout.*;
 import static main.io.util.ChartUtil.balanceSeries;
 import static main.io.util.ChartUtil.dateAxis;
@@ -28,9 +31,13 @@ import static main.io.util.ChartUtil.dateAxis;
 public class HistoryController extends EventConnection implements Initializable, NavListener, MemoryListener, TransactionListener {
 
     @FXML
+    public DatePicker datePicker;
+
+    @FXML
     public GridPane chartHousing;
 
     private List<Transaction> allTransactions = new LinkedList<>();
+    private Account account;
 
     @FXML
     public void onHome() {
@@ -39,8 +46,32 @@ public class HistoryController extends EventConnection implements Initializable,
     }
 
     @FXML
-    public void onDateSelected(ActionEvent event) {
-        // TODO: how to get the date?
+    public void onDateSelected() {
+        final List<Transaction> accountTransactions = listForAccount(account.getKey(), allTransactions);
+        final LocalDate date = datePicker.getValue();
+
+        chartHousing.getChildren().clear();
+
+        if (date != null) {
+            drawChart(
+                    account,
+                    dateAxis(listForAccount(account.getKey(), allTransactions), date),
+                    new NumberAxis(),
+                    balanceSeries(accountTransactions, date)
+            );
+        }
+    }
+
+    @FXML
+    public void clearFilter() {
+        final List<Transaction> accountTransactions = listForAccount(account.getKey(), allTransactions);
+        datePicker.setValue(null);
+        drawChart(
+                account,
+                dateAxis(listForAccount(account.getKey(), allTransactions)),
+                new NumberAxis(),
+                balanceSeries(accountTransactions)
+        );
     }
 
     @Override
@@ -49,17 +80,14 @@ public class HistoryController extends EventConnection implements Initializable,
     @Override
     public void onChangeLayout(Layout layout, Account account) {
         if (layout == HISTORY) {
-            final List<Transaction> accountTransactions = Transaction.listForAccount(account.getKey(), allTransactions);
-            final Axis<String> xAxis = dateAxis(accountTransactions);
-            final NumberAxis yAxis = new NumberAxis();
-            final LineChart<String, Number> chart = new LineChart<>(xAxis, yAxis);
-
-            chart.setPrefWidth(1200);
-            chart.setPrefHeight(800);
-            chart.setTitle(String.format("%s Transactions: %s", account.getName(), accountTransactions.size()));
-            chart.getData().add(balanceSeries(accountTransactions));
-
-            chartHousing.add(chart, 0, 0);
+            this.account = account;
+            final List<Transaction> accountTransactions = listForAccount(account.getKey(), allTransactions);
+            drawChart(
+                    account,
+                    dateAxis(listForAccount(account.getKey(), allTransactions)),
+                    new NumberAxis(),
+                    balanceSeries(accountTransactions)
+            );
         }
     }
 
@@ -83,4 +111,16 @@ public class HistoryController extends EventConnection implements Initializable,
 
     @Override
     public void onAccountsLoaded(Collection<Account> accounts) {}
+
+    public void drawChart(Account account, Axis<String> xAxis, NumberAxis yAxis, XYChart.Series<String, Number> series) {
+        final List<Transaction> accountTransactions = listForAccount(account.getKey(), allTransactions);
+        final LineChart<String, Number> chart = new LineChart<>(xAxis, yAxis);
+
+        chart.setPrefWidth(1200);
+        chart.setPrefHeight(800);
+        chart.setTitle(String.format("%s Transactions: %s", account.getName(), accountTransactions.size()));
+        chart.getData().add(series);
+
+        chartHousing.add(chart, 0, 0);
+    }
 }

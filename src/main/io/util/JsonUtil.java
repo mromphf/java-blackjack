c1f4a1018f7fd7a64d9f4a1361cfe4bb5a9385f5
Mark Romphf <markromphf@gmail.com>
2021-03-12 14:34:13 -0600

    Remove unneeded methods and dead code.

--- src/main/io/util/JsonUtil.java
+++ src/main/io/util/JsonUtil.java
@@ -2,21 +2,11 @@ package main.io.util;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
-import main.Config;
-import main.domain.Account;
 import main.domain.Card;
 
 import java.util.Stack;
 
 public class JsonUtil {
-
-    public static Account accountFromJson(String jsonDocument) {
-        return new Gson().fromJson(jsonDocument, Account.class);
-    }
-
-    public static Config configFromJson(String jsonDocument) {
-        return new Gson().fromJson(jsonDocument, Config.class);
-    }
 
     public static Stack<Card> deckFromJson(String jsonDocument) {
         final Stack<Card> result = new Stack<>();

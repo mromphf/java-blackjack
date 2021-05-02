ff2bf0fd8e6679b2372febb696ee33e30303871b
Mark Romphf <markromphf@gmail.com>
2021-05-02 15:47:21 -0600

    Fix deck deserialization.

--- src/main/io/util/JsonUtil.java
+++ src/main/io/util/JsonUtil.java
@@ -3,15 +3,23 @@ package main.io.util;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import main.domain.Card;
+import main.domain.Suit;
 
 import java.util.Stack;
 
 public class JsonUtil {
 
-    public static Stack<Card> deckFromJson(String jsonDocument) {
+    public static Stack<Card> deckFromJson(String jsonString) {
         final Stack<Card> result = new Stack<>();
-        final Gson gson = new Gson();
-        final JsonObject json = gson.toJsonTree(jsonDocument).getAsJsonObject();
+        final JsonObject[] jsonObjects = new Gson().fromJson(jsonString, JsonObject[].class);
+
+        for (JsonObject obj : jsonObjects) {
+            final int val = obj.get("value").getAsInt();
+            final Suit suit = Suit.valueOf(obj.get("suit").getAsString().toUpperCase());
+
+            result.add(new Card(val, suit));
+        }
+
         return result;
     }
 }

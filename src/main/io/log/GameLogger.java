c095fcbe81a98a008be8420956a02abb6785ec18
Mark Romphf <markromphf@gmail.com>
2021-05-06 19:41:25 -0600

    GameLogger now implements AccountListener.

--- src/main/io/log/GameLogger.java
+++ src/main/io/log/GameLogger.java
@@ -2,6 +2,7 @@ package main.io.log;
 
 import main.domain.Account;
 import main.domain.Snapshot;
+import main.usecase.AccountListener;
 import main.usecase.GameStateListener;
 import main.usecase.BalanceListener;
 
@@ -9,7 +10,7 @@ import java.time.LocalTime;
 import java.util.logging.Logger;
 import static java.util.logging.Level.*;
 
-public class GameLogger extends Logger implements GameStateListener, BalanceListener {
+public class GameLogger extends Logger implements GameStateListener, BalanceListener, AccountListener {
 
     public GameLogger(String name, String resourceBundleName) {
         super(name, resourceBundleName);
@@ -23,5 +24,16 @@ public class GameLogger extends Logger implements GameStateListener, BalanceList
     @Override
     public void onBalanceUpdated(Account account) {
         log(INFO, String.format("%s: %s's Balance: %s", LocalTime.now(), account.getName(), account.getBalance()));
+    }
+
+    @Override
+    public void onNewAccountOpened(Account account) {
+        log(INFO, String.format(
+                "%s: Account no. %s has been opened under the name %s.", LocalTime.now(), account.getKey(), account.getName()));
+    }
+
+    @Override
+    public void onAccountDeleted(Account account) {
+        log(INFO, String.format("%s: Request issued to close account no. %s.", LocalTime.now(), account.getKey()));
     }
 }

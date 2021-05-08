60d53a792ee9e7e187e1ea2649c93df7e382702e
Mark Romphf <markromphf@gmail.com>
2021-05-08 16:29:58 -0600

    Add a History button to the Bet screen.

--- src/main/usecase/LayoutManager.java
+++ src/main/usecase/LayoutManager.java
@@ -11,19 +11,20 @@ import static main.usecase.Layout.BACK;
 
 public class LayoutManager extends EventConnection implements NavListener, BalanceListener {
 
-    private final Queue<Layout> navHistory;
+    private final Stack<Layout> navHistory;
     private final Scene scene;
     private final Map<Layout, Parent> layoutMap;
 
     public LayoutManager(Scene scene, Map<Layout, Parent> layoutMap) {
         this.scene = scene;
         this.layoutMap = layoutMap;
-        this.navHistory = new LinkedList<>();
+        this.navHistory = new Stack<>();
     }
 
     @Override
     public void onChangeLayout(Layout layout) {
         if (layout == BACK) {
+            navHistory.pop();
             scene.setRoot(layoutMap.get(navHistory.peek()));
         } else {
             navHistory.add(layout);

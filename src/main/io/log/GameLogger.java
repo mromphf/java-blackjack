b80ef4c80660c88e61a16f1bbc1eb7b8a6b82b9d
Mark Romphf <markromphf@gmail.com>
2021-06-09 19:06:41 -0600

    Playing with log formatting.

--- src/main/io/log/GameLogger.java
+++ src/main/io/log/GameLogger.java
@@ -9,11 +9,14 @@ import main.usecase.BalanceListener;
 import main.usecase.TransactionListener;
 
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.logging.Logger;
 import static java.util.logging.Level.*;
 
 public class GameLogger extends Logger implements GameStateListener, BalanceListener, AccountListener, TransactionListener {
+
+    private final DateTimeFormatter pattern = DateTimeFormatter.ofPattern("kk:mm:ss");
 
     public GameLogger(String name, String resourceBundleName) {
         super(name, resourceBundleName);
@@ -21,29 +24,29 @@ public class GameLogger extends Logger implements GameStateListener, BalanceList
 
     @Override
     public void onUpdate(Snapshot snapshot) {
-        log(INFO, String.format("%s: SNAPSHOT - %s", snapshot.getTimestamp().toLocalTime(), snapshot));
+        log(INFO, String.format("%s: SNAPSHOT - %s", snapshot.getTimestamp().toLocalTime().format(pattern), snapshot));
     }
 
     @Override
     public void onBalanceUpdated(Account account) {
-        log(INFO, String.format("%s: BALANCE - %s ($%s)", LocalTime.now(), account.getName(), account.getBalance()));
+        log(INFO, String.format("%s: BALANCE - %s ($%s)", LocalTime.now().format(pattern), account.getName(), account.getBalance()));
     }
 
     @Override
     public void onNewAccountOpened(Account account) {
         log(INFO, String.format(
-                "%s: Account no. %s has been opened under the name %s.", LocalTime.now(), account.getKey(), account.getName()));
+                "%s: Account no. %s has been opened under the name %s.", LocalTime.now().format(pattern), account.getKey(), account.getName()));
     }
 
     @Override
     public void onAccountDeleted(Account account) {
-        log(INFO, String.format("%s: Request issued to close account no. %s.", LocalTime.now(), account.getKey()));
+        log(INFO, String.format("%s: Request issued to close account no. %s.", LocalTime.now().format(pattern), account.getKey()));
     }
 
     @Override
     public void onTransaction(Transaction transaction) {
         log(INFO, String.format("%s: TRANSACTION - %s ($%s) Account Key: %s",
-                transaction.getTime().toLocalTime(),
+                transaction.getTime().toLocalTime().format(pattern),
                 transaction.getDescription(),
                 transaction.getAmount(),
                 transaction.getAccountKey()));

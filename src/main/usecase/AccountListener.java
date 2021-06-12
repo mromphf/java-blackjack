058abd51d569be5cb63d1132fe3d90c3c5240c93
Mark Romphf <markromphf@gmail.com>
2021-06-12 15:45:35 -0600

    Delete AccountListener.

--- src/main/usecase/AccountListener.java
+++
@@ -1,9 +0,0 @@
-package main.usecase;
-
-import main.domain.Account;
-
-
-public interface AccountListener {
-    void onNewAccountOpened(Account account);
-    void onAccountDeleted(Account account);
-}

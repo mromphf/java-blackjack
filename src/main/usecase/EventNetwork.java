59e48ac547464af28a5c2184229bc122a00ed81b
Mark Romphf <markromphf@gmail.com>
2021-06-12 16:03:32 -0600

    Replace 'MemoryListener' with 'EventListener'

--- src/main/usecase/EventNetwork.java
+++ src/main/usecase/EventNetwork.java
@@ -12,7 +12,6 @@ public class EventNetwork implements
         ActionListener,
         BalanceListener,
         GameStateListener,
-        MemoryListener,
         NavListener,
         TransactionListener,
         Responder,
@@ -21,7 +20,6 @@ public class EventNetwork implements
     private final Collection<ActionListener> actionListeners = new LinkedList<>();
     private final Collection<BalanceListener> balanceListeners = new LinkedList<>();
     private final Collection<GameStateListener> gameStateListeners = new LinkedList<>();
-    private final Collection<MemoryListener> memoryListeners = new LinkedList<>();
     private final Collection<NavListener> navListeners = new LinkedList<>();
     private final Collection<TransactionListener> transactionListeners = new LinkedList<>();
     private final Collection<EventListener> eventListeners = new LinkedList<>();
@@ -39,10 +37,6 @@ public class EventNetwork implements
 
             if (connection instanceof GameStateListener) {
                 gameStateListeners.add((GameStateListener) connection);
-            }
-
-            if (connection instanceof MemoryListener) {
-                memoryListeners.add((MemoryListener) connection);
             }
 
             if (connection instanceof NavListener) {
@@ -102,16 +96,6 @@ public class EventNetwork implements
     @Override
     public void onUpdate(Snapshot snapshot) {
         gameStateListeners.forEach(l -> l.onUpdate(snapshot));
-    }
-
-    @Override
-    public void onAccountsLoaded(Collection<Account> accounts) {
-        memoryListeners.forEach(l -> l.onAccountsLoaded(accounts));
-    }
-
-    @Override
-    public void onTransactionsLoaded(List<Transaction> transactions) {
-        memoryListeners.forEach(l -> l.onTransactionsLoaded(transactions));
     }
 
     @Override

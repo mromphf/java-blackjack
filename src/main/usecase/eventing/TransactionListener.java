20149f2ef6a0be27248febd2e1f4c26fc0b501c7
Mark Romphf <markromphf@gmail.com>
2021-06-18 20:46:19 -0600

    Include default implementation of TransactionListener.onTransactionEvent.

--- src/main/usecase/eventing/TransactionListener.java
+++ src/main/usecase/eventing/TransactionListener.java
@@ -2,9 +2,16 @@ package main.usecase.eventing;
 
 import main.domain.Transaction;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public interface TransactionListener {
-    void onTransactionEvent(Event<Transaction> event);
+
     void onTransactionsEvent(Event<Collection<Transaction>> event);
+
+    default void onTransactionEvent(Event<Transaction> event) {
+        Collection<Transaction> transactions = new ArrayList<>();
+        transactions.add(event.getData());
+        onTransactionsEvent(new Event<>(event.getTimestamp(), event.getPredicate(), transactions));
+    }
 }

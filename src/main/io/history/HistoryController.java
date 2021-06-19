10751dd635e321970c27d8b9dc16e243745f6ec6
Mark Romphf <markromphf@gmail.com>
2021-06-18 21:00:27 -0600

    Move the in-memory cache of Transactions from the HistoryController to the Transactor.

--- src/main/io/history/HistoryController.java
+++ src/main/io/history/HistoryController.java
@@ -17,28 +17,21 @@ import main.usecase.eventing.*;
 import java.net.URL;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Collection;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.ResourceBundle;
+import java.util.*;
 
-import static main.domain.Transaction.listForAccount;
 import static main.io.util.ChartUtil.balanceSeries;
 import static main.io.util.ChartUtil.dateAxis;
 import static main.usecase.Layout.BACK;
 import static main.usecase.Layout.HISTORY;
 import static main.usecase.eventing.Predicate.*;
 
-public class HistoryController extends EventConnection implements Initializable, LayoutListener, TransactionListener {
+public class HistoryController extends EventConnection implements Initializable, LayoutListener {
 
     @FXML
     public DatePicker datePicker;
 
     @FXML
     public GridPane chartHousing;
-
-    private final List<Transaction> allTransactions = new LinkedList<>();
-    private Account account;
 
     @FXML
     public void onBack() {
@@ -49,7 +42,9 @@ public class HistoryController extends EventConnection implements Initializable,
 
     @FXML
     public void onDateSelected() {
-        final List<Transaction> accountTransactions = listForAccount(account.getKey(), allTransactions);
+        final Account account = eventNetwork.requestSelectedAccount(ACCOUNT_SELECTED);
+        final List<Transaction> accountTransactions = new ArrayList<>(
+                eventNetwork.requestTransactionsByKey(account.getKey()));
         final LocalDate date = datePicker.getValue();
         final NumberAxis yAxis = new NumberAxis();
         final Axis<String> xAxis = date == null ? dateAxis(accountTransactions) : dateAxis(accountTransactions, date);
@@ -63,7 +58,9 @@ public class HistoryController extends EventConnection implements Initializable,
 
     @FXML
     public void clearFilter() {
-        final List<Transaction> accountTransactions = listForAccount(account.getKey(), allTransactions);
+        final Account account = eventNetwork.requestSelectedAccount(ACCOUNT_SELECTED);
+        final List<Transaction> accountTransactions = new ArrayList<>(
+                eventNetwork.requestTransactionsByKey(account.getKey()));
         final NumberAxis yAxis = new NumberAxis();
         final Axis<String> xAxis = dateAxis(accountTransactions);
         final XYChart.Series<String, Number> balanceSeries = balanceSeries(accountTransactions);
@@ -77,40 +74,22 @@ public class HistoryController extends EventConnection implements Initializable,
     public void initialize(URL location, ResourceBundle resources) {}
 
     @Override
-    public void onTransactionEvent(Event<Transaction> event) {
-        if (event.is(TRANSACTION)) {
-            allTransactions.add(event.getData());
-        }
-    }
-
-    @Override
-    public void onTransactionsEvent(Event<Collection<Transaction>> event) {
-        if (event.is(TRANSACTIONS_LOADED) || event.is(TRANSACTION_SERIES)) {
-            allTransactions.addAll(event.getData());
-        }
-    }
-
-    @Override
     public void onLayoutEvent(Event<Layout> event) {
+        final Account account = eventNetwork.requestSelectedAccount(ACCOUNT_SELECTED);
+        final List<Transaction> transactions = new ArrayList<>(eventNetwork.requestTransactionsByKey(account.getKey()));
+
         if (event.is(LAYOUT_CHANGED) && event.getData() == HISTORY) {
-            this.account = eventNetwork.requestSelectedAccount(ACCOUNT_SELECTED);
-            final List<Transaction> accountTransactions = listForAccount(account.getKey(), allTransactions);
-            drawChart(
-                    account,
-                    dateAxis(listForAccount(account.getKey(), allTransactions)),
-                    new NumberAxis(),
-                    balanceSeries(accountTransactions)
-            );
+            drawChart(account, dateAxis(transactions), new NumberAxis(), balanceSeries(transactions));
         }
     }
 
     public void drawChart(Account account, Axis<String> xAxis, NumberAxis yAxis, XYChart.Series<String, Number> series) {
-        final List<Transaction> accountTransactions = listForAccount(account.getKey(), allTransactions);
+        final List<Transaction> transactions = new ArrayList<>(eventNetwork.requestTransactionsByKey(account.getKey()));
         final LineChart<String, Number> chart = new LineChart<>(xAxis, yAxis);
 
         chart.setPrefWidth(1200);
         chart.setPrefHeight(800);
-        chart.setTitle(String.format("%s Transactions: %s", account.getName(), accountTransactions.size()));
+        chart.setTitle(String.format("%s Transactions: %s", account.getName(), transactions.size()));
         chart.getData().add(series);
 
         chartHousing.add(chart, 0, 0);

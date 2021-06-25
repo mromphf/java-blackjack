d2fec5d4c8ce9bf957c7e6bb37eb1c10da3c86a3
Mark Romphf <markromphf@gmail.com>
2021-06-24 20:19:29 -0600

    Add UUID field to the Event class.

--- src/main/usecase/eventing/Event.java
+++ src/main/usecase/eventing/Event.java
@@ -1,13 +1,23 @@
 package main.usecase.eventing;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 public class Event<T> {
+    private final UUID sourceKey;
     private final LocalDateTime timestamp;
     private final Predicate predicate;
     private final T data;
 
     public Event(LocalDateTime timestamp, Predicate predicate, T data) {
+        this.sourceKey = UUID.randomUUID();
+        this.timestamp = timestamp;
+        this.predicate = predicate;
+        this.data = data;
+    }
+
+    public Event(UUID sourceKey, LocalDateTime timestamp, Predicate predicate, T data) {
+        this.sourceKey = sourceKey;
         this.timestamp = timestamp;
         this.predicate = predicate;
         this.data = data;
@@ -15,6 +25,10 @@ public class Event<T> {
 
     public boolean is(Predicate p) {
         return predicate.equals(p);
+    }
+
+    public UUID getSourceKey() {
+        return sourceKey;
     }
 
     public LocalDateTime getTimestamp() {

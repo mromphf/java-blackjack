91d2183bad17447bb57e0e2211fdd83cd7fa04f4
Mark Romphf <markromphf@gmail.com>
2021-06-25 19:18:29 -0600

    Cleanup in the TransactionListener.

--- src/main/usecase/eventing/TransactionListener.java
+++ src/main/usecase/eventing/TransactionListener.java
@@ -3,16 +3,23 @@ package main.usecase.eventing;
 import main.domain.Identifiable;
 import main.domain.Transaction;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.UUID;
 
 public interface TransactionListener extends Identifiable {
 
     void onTransactionsEvent(Event<Collection<Transaction>> event);
 
     default void onTransactionEvent(Event<Transaction> event) {
-        Collection<Transaction> transactions = new ArrayList<>();
+        final Collection<Transaction> transactions = new ArrayList<>();
+        final UUID sourceKey = event.getKey();
+        final LocalDateTime timestamp = event.getTimestamp();
+        final Predicate predicate = event.getPredicate();
+
         transactions.add(event.getData());
-        onTransactionsEvent(new Event<>(event.getKey(), event.getTimestamp(), event.getPredicate(), transactions));
+
+        onTransactionsEvent(new Event<>(sourceKey, timestamp, predicate, transactions));
     }
 }

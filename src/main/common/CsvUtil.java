fa1f95d936fa57e24876717c00d79f9cbc2229f9
Mark Romphf <markromphf@gmail.com>
2021-07-01 15:20:35 -0600

    CsvUtil method for printing account closure rows.

--- src/main/common/CsvUtil.java
+++ src/main/common/CsvUtil.java
@@ -3,12 +3,16 @@ package main.common;
 import main.domain.Account;
 import main.domain.Transaction;
 
+import java.time.LocalDateTime;
+
+import static java.time.LocalDateTime.*;
 import static java.time.ZoneId.systemDefault;
 import static java.time.format.DateTimeFormatter.ISO_OFFSET_DATE_TIME;
 
 public class CsvUtil {
 
     public static final String ACCOUNT_HEADER = "key,name,created";
+    public static final String ACCOUNT_CLOSURE_HEADER = "timestamp, accountKey";
     public static final String TRANSACTION_HEADER = "time,accountKey,description,amount";
 
     public static String toCsvRow(Account account) {
@@ -25,5 +29,10 @@ public class CsvUtil {
                 transaction.getAccountKey(),
                 transaction.getDescription(),
                 transaction.getAmount());
+    }
+
+    public static String accountClosureRow(Account account) {
+        final String zonedTimestamp = now().atZone(systemDefault()).format(ISO_OFFSET_DATE_TIME);
+        return String.format("%s,%s", zonedTimestamp, account.getKey());
     }
 }

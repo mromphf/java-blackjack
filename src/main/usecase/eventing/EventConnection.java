4ae0b7fa385b4000b80f5a7a6c69e13edd533379
Mark Romphf <markromphf@gmail.com>
2021-07-11 11:58:58 -0600

    Initialize event connections against an empty network to protect against NullPointerExceptions.

--- src/main/usecase/eventing/EventConnection.java
+++ src/main/usecase/eventing/EventConnection.java
@@ -1,8 +1,12 @@
 package main.usecase.eventing;
 
+import java.util.LinkedList;
+
+import static java.util.UUID.randomUUID;
+
 public abstract class EventConnection {
 
-    protected EventNetwork eventNetwork;
+    protected EventNetwork eventNetwork = new EventNetwork(randomUUID(), new LinkedList<>());
 
     public void connectTo(EventNetwork eventNetwork) {
         this.eventNetwork = eventNetwork;

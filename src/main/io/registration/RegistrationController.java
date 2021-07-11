e449556baebe3fcfdb65c1bd4424de5eecb442e2
Mark Romphf <markromphf@gmail.com>
2021-07-11 11:41:19 -0600

    Add package for account registration.

---
+++ src/main/io/registration/RegistrationController.java
@@ -0,0 +1,14 @@
+package main.io.registration;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class RegistrationController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}

2c8401c7bec0185a611c40a7ed3309889dc2922a
Mark Romphf <markromphf@gmail.com>
2021-07-25 13:21:54 -0600

    Script will group transaction data by account key.

--- scripts/group_transactions_by_account.py
+++ scripts/group_transactions_by_account.py
@@ -1,9 +1,10 @@
-#!usr/bin/python
+#!usr/local/bin/python3.9
 
 import os
+import pandas as pd
 from csv import DictReader
-from dateutil import parser
-from uuid import UUID
+
+TRANSACTIONS_DIR = "../app-data/transactions/"
 
 
 def compile_csv_files(csv_files):
@@ -11,16 +12,18 @@ def compile_csv_files(csv_files):
     for file in csv_files:
         with open(file, 'r') as csv_contents:
             for csv_row in DictReader(csv_contents):
-                dictionaries.append({key:csv_row[key] for key in csv_row.keys()})
+                dictionaries.append({key: csv_row[key] for key in csv_row.keys()})
     return dictionaries
 
 
 def qualified_file_name(filename):
-    return "../app-data/transactions/" + filename
+    return TRANSACTIONS_DIR + filename
 
 
 if __name__ == "__main__":
-    csv_files = os.listdir("../app-data/transactions")
-    dicts = compile_csv_files(map(qualified_file_name, csv_files))
-    for d in dicts:
-        print d
+    files = os.listdir(TRANSACTIONS_DIR)
+    dicts = compile_csv_files(map(qualified_file_name, files))
+    df = pd.DataFrame(dicts)
+    for account_key in list(df["accountKey"].unique()):
+        filename = f"../app-data/transactions-grouped/{account_key}.csv"
+        df[df['accountKey'] == account_key].to_csv(filename, index=False)

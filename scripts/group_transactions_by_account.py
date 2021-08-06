ca03b6872e98f6c8d3758f1b3e5a09c254d934a9
Mark Romphf <markromphf@gmail.com>
2021-08-05 19:56:08 -0600

    Generalize the python file-grouping script.

--- scripts/group_transactions_by_account.py
+++ scripts/group_transactions_by_account.py
@@ -4,7 +4,9 @@ import os
 import pandas as pd
 from csv import DictReader
 
-TRANSACTIONS_DIR = "../app-data/transactions/"
+SOURCE_DIR = "../app-data/accounts/"
+GROUPED_DIR = "../app-data/accounts-grouped/"
+KEY = "key"
 
 
 def compile_csv_files(csv_files):
@@ -17,13 +19,13 @@ def compile_csv_files(csv_files):
 
 
 def qualified_file_name(filename):
-    return TRANSACTIONS_DIR + filename
+    return SOURCE_DIR + filename
 
 
 if __name__ == "__main__":
-    files = os.listdir(TRANSACTIONS_DIR)
+    files = os.listdir(SOURCE_DIR)
     dicts = compile_csv_files(map(qualified_file_name, files))
     df = pd.DataFrame(dicts)
-    for account_key in list(df["accountKey"].unique()):
-        filename = f"../app-data/transactions-grouped/{account_key}.csv"
-        df[df['accountKey'] == account_key].to_csv(filename, index=False)
+    for account_key in list(df[KEY].unique()):
+        filename = f"{GROUPED_DIR}{account_key}.csv"
+        df[df[KEY] == account_key].to_csv(filename, index=False)

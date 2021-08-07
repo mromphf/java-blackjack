2e42ea459aa7fb20fc7b08938d80218d089c8700
Mark Romphf <markromphf@gmail.com>
2021-08-07 17:09:15 -0600

    Add scrolling animation to the Bet screen.

--- src/main/io/bet/BetController.java
+++ src/main/io/bet/BetController.java
@@ -2,6 +2,8 @@ package main.io.bet;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
@@ -10,6 +12,7 @@ import javafx.scene.input.MouseEvent;
 import main.domain.Account;
 import main.domain.Bet;
 import main.domain.Snapshot;
+import main.io.graphics.ScrollingImageReelAnimation;
 import main.usecase.eventing.AccountListener;
 import main.usecase.eventing.Event;
 import main.usecase.eventing.EventConnection;
@@ -29,6 +32,9 @@ import static main.usecase.eventing.Predicate.*;
 
 
 public class BetController extends EventConnection implements Initializable, AccountListener, SnapshotListener {
+
+    @FXML
+    private Canvas cvsScroller;
 
     @FXML
     private ProgressBar prgDeck;
@@ -59,11 +65,14 @@ public class BetController extends EventConnection implements Initializable, Acc
 
     private final static int MAX_BET = 500;
     private final UUID key = randomUUID();
+    private ScrollingImageReelAnimation animation;
     private int bet = 0;
     private int balance = 0;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        final GraphicsContext graphics = cvsScroller.getGraphicsContext2D();
+
         prgDeck.setProgress(100);
 
         btnBet1.setOnMouseClicked(event -> onBet(event, 1));
@@ -71,6 +80,10 @@ public class BetController extends EventConnection implements Initializable, Acc
         btnBet10.setOnMouseClicked(event -> onBet(event, 10));
         btnBet25.setOnMouseClicked(event -> onBet(event, 25));
         btnBet100.setOnMouseClicked(event -> onBet(event, 100));
+
+        animation = new ScrollingImageReelAnimation(graphics, true);
+
+        new Thread(() -> animation.start(), "Bet Screen Animation Thread").start();
     }
 
     @FXML
@@ -98,6 +111,11 @@ public class BetController extends EventConnection implements Initializable, Acc
     @FXML
     public void onHistory() {
         eventNetwork.onLayoutEvent(new Event<>(key, now(), LAYOUT_CHANGED, HISTORY));
+    }
+
+    @FXML
+    public void onClickScroller() {
+        animation.switchDirection();
     }
 
     @Override

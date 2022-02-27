e8b0c68f3142610ee81fca03e6d0789618092f36
Mark Romphf <markromphf@gmail.com>
2022-02-26 18:45:19 -0600

    Remove loadDeck() from the TransactionMemory interface.

--- src/main/io/storage/TransactionMemory.java
+++ src/main/io/storage/TransactionMemory.java
@@ -1,15 +1,12 @@
 package main.io.storage;
 
 import main.domain.Account;
-import main.domain.Card;
 import main.domain.Transaction;
 
 import java.util.Collection;
-import java.util.Stack;
 
 public interface TransactionMemory {
-    Stack<Card> loadDeck(String name);
-    Collection<Transaction> loadAllTransactions(Collection<Account> openAccoutns);
+    Collection<Transaction> loadAllTransactions(Collection<Account> openAccounts);
     void saveTransaction(Transaction transaction);
     default void saveTransactions(Collection<Transaction> transactions) {
         transactions.forEach(this::saveTransaction);

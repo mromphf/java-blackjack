d5e20668c3f7409fe56ca07e6a083927b1d794f0
Mark Romphf <markromphf@gmail.com>
2022-03-07 13:45:45 -0600

    Push injector construction up into Main.

--- src/main/AppRoot.java
+++ src/main/AppRoot.java
@@ -1,42 +1,34 @@
 package main;
 
 import com.google.inject.Injector;
-import javafx.fxml.FXMLLoader;
-import javafx.stage.Stage;
 import main.adapter.bet.BetController;
 import main.adapter.blackjack.BlackjackController;
 import main.adapter.history.HistoryController;
 import main.adapter.home.HomeController;
-import main.adapter.injection.BaseInjectionModule;
-import main.adapter.injection.ConfigInjectionModule;
-import main.adapter.injection.FXMLInjectionModule;
 import main.adapter.log.GameLogger;
 import main.adapter.registration.RegistrationController;
 import main.adapter.storage.AccountStorage;
-import main.adapter.storage.Directory;
-import main.adapter.storage.FileSystem;
 import main.usecase.*;
 import main.usecase.eventing.EventConnection;
 import main.usecase.eventing.EventNetwork;
 
-import java.io.File;
 import java.util.Collection;
 import java.util.LinkedList;
-import java.util.Map;
-
-import static com.google.inject.Guice.createInjector;
 
 public class AppRoot {
 
-    public AppRoot(Stage stage,
-                   Map<Directory, File> directoryMap,
-                   Map<Layout, FXMLLoader> resourceMap,
-                   FileSystem fileSystem) {
+    private final Injector configInjector;
+    private final Injector baseInjector;
+    private final Injector fxmlInjector;
 
-        final Injector configInjector = createInjector(new ConfigInjectionModule(fileSystem));
-        final Injector baseInjector = createInjector(new BaseInjectionModule(directoryMap));
-        final Injector fxmlInjection = createInjector(new FXMLInjectionModule(stage, baseInjector, resourceMap));
+    public AppRoot(Injector configInjector, Injector baseInjector, Injector fxmlInjector) {
+        this.configInjector = configInjector;
+        this.baseInjector = baseInjector;
+        this.fxmlInjector = fxmlInjector;
+    }
 
+
+    public void init() {
         final Game game = configInjector.getInstance(Game.class);
 
         final AccountStorage accountStorage = baseInjector.getInstance(AccountStorage.class);
@@ -46,12 +38,12 @@ public class AppRoot {
         final Transactor transactor = baseInjector.getInstance(Transactor.class);
         final EventNetwork eventNetwork = baseInjector.getInstance(EventNetwork.class);
 
-        final LayoutManager layoutManager = fxmlInjection.getInstance(LayoutManager.class);
-        final HomeController homeController = fxmlInjection.getInstance(HomeController.class);
-        final HistoryController historyController = fxmlInjection.getInstance(HistoryController.class);
-        final BlackjackController blackjackController = fxmlInjection.getInstance(BlackjackController.class);
-        final BetController betController = fxmlInjection.getInstance(BetController.class);
-        final RegistrationController registrationController = fxmlInjection.getInstance(RegistrationController.class);
+        final LayoutManager layoutManager = fxmlInjector.getInstance(LayoutManager.class);
+        final HomeController homeController = fxmlInjector.getInstance(HomeController.class);
+        final HistoryController historyController = fxmlInjector.getInstance(HistoryController.class);
+        final BlackjackController blackjackController = fxmlInjector.getInstance(BlackjackController.class);
+        final BetController betController = fxmlInjector.getInstance(BetController.class);
+        final RegistrationController registrationController = fxmlInjector.getInstance(RegistrationController.class);
 
         final Collection<EventConnection> eventConnections = new LinkedList<EventConnection>() {{
             add(accountCache);
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,15 @@
 package main;
 
+import com.google.inject.Injector;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import main.adapter.blackjack.ImageMap;
+import main.adapter.injection.BaseInjectionModule;
+import main.adapter.injection.ConfigInjectionModule;
+import main.adapter.injection.FXMLInjectionModule;
 import main.adapter.storage.FileSystem;
 
+import static com.google.inject.Guice.createInjector;
 import static java.lang.Thread.currentThread;
 import static main.adapter.storage.FileSystem.directoryMap;
 import static main.adapter.storage.FileSystem.resourceMap;
@@ -13,12 +18,20 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) {
-        new AppRoot(stage, directoryMap(), resourceMap(), new FileSystem(directoryMap()));
+       final Injector configInjector = createInjector(new ConfigInjectionModule(new FileSystem(directoryMap())));
+       final Injector baseInjector = createInjector(new BaseInjectionModule(directoryMap()));
+       final Injector fxmlInjector = createInjector(new FXMLInjectionModule(stage, baseInjector, resourceMap()));
+
+       final AppRoot appRoot = new AppRoot(configInjector, baseInjector, fxmlInjector);
+
+        appRoot.init();
     }
 
     public static void main(String[] args) {
         currentThread().setName("Main Thread");
+
         ImageMap.load();
+
         launch(args);
     }
 }

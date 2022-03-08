a52364a18e5ece0f893b82f7f9dc5fbaa2c38ef4
Mark Romphf <markromphf@gmail.com>
2022-03-08 16:25:18 -0600

    Not using the FileSystem for persistence.

--- src/main/adapter/storage/FileFunctions.java
+++ src/main/adapter/storage/FileFunctions.java
@@ -1,28 +1,17 @@
 package main.adapter.storage;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Objects;
 import java.util.Scanner;
 
 import static java.lang.String.format;
 import static java.time.format.DateTimeFormatter.ISO_DATE;
 
 public class FileFunctions {
-    public static String dateBasedCsvFileName(LocalDateTime t) {
-        return format("%s.csv", t.format(ISO_DATE));
-    }
 
     public static String dateBasedLogFileName(LocalDateTime t) {
         return format("%s.log", t.format(ISO_DATE));
-    }
-
-    public static File[] allFilesInDir(File directory) {
-        return Objects.requireNonNull(directory.listFiles());
     }
 
     public static String fileToJson(File f) throws IOException {
@@ -36,25 +25,4 @@ public class FileFunctions {
         return sb.toString();
     }
 
-    public static Collection<String> readCsvLines(File csvFile)  {
-        final Scanner sc;
-        final Collection<String> strings = new ArrayList<>();
-
-        try {
-            sc = new Scanner(csvFile);
-
-            if (sc.hasNextLine()) {
-                // Skip header row
-                sc.nextLine();
-            }
-
-            while (sc.hasNextLine()) {
-                strings.add(sc.nextLine());
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        return strings;
-    }
 }
--- src/main/common/CsvUtil.java
+++ src/main/common/CsvUtil.java
@@ -1,95 +1,11 @@
 package main.common;
 
-import main.domain.Account;
-import main.domain.Transaction;
-
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.time.LocalDateTime;
-import java.time.ZonedDateTime;
-import java.util.Map;
-import java.util.SortedMap;
-import java.util.TreeMap;
-import java.util.UUID;
-
-import static java.time.LocalDateTime.now;
-import static java.time.ZoneId.systemDefault;
-import static java.time.format.DateTimeFormatter.ISO_OFFSET_DATE_TIME;
 
 public class CsvUtil {
-
-    public static final String ACCOUNT_HEADER = "key,name,created";
-    public static final String ACCOUNT_CLOSURE_HEADER = "timestamp, accountKey";
-    public static final String TRANSACTION_HEADER = "time,accountKey,description,amount";
-
-    public static String toCsvRow(Account account) {
-        final String zonedTimestamp = account.getCreated().atZone(systemDefault()).format(ISO_OFFSET_DATE_TIME);
-
-        return String.format("%s,%s,%s", account.getKey(), account.getName(), zonedTimestamp);
-    }
-
-    public static String toCsvRow(Transaction transaction) {
-        final String zonedTimestamp = transaction.getTime().atZone(systemDefault()).format(ISO_OFFSET_DATE_TIME);
-
-        return String.format("%s,%s,%s,%s",
-                zonedTimestamp,
-                transaction.getAccountKey(),
-                transaction.getDescription(),
-                transaction.getAmount());
-    }
-
-    public static String accountClosureRow(Account account) {
-        final String zonedTimestamp = now().atZone(systemDefault()).format(ISO_OFFSET_DATE_TIME);
-        return String.format("%s,%s", zonedTimestamp, account.getKey());
-    }
-
-    public static Account accountFromCsvRow(String[] delineatedRow) {
-        return new Account(
-                UUID.fromString(delineatedRow[0]),
-                delineatedRow[1],
-                0,
-                ZonedDateTime.parse(delineatedRow[2]).toLocalDateTime()
-        );
-    }
-
-    public static Transaction transactionsFromCsvRow(String[] delineatedRow) {
-        return new Transaction(
-                ZonedDateTime.parse(delineatedRow[0]).toLocalDateTime(),
-                UUID.fromString(delineatedRow[1]),
-                delineatedRow[2],
-                Integer.parseInt(delineatedRow[3])
-        );
-    }
-
-    public static SortedMap<LocalDateTime, UUID> accountClosuresFromCsvRow(String[] delineatedRow) {
-        final SortedMap<LocalDateTime, UUID> timestampedClosures = new TreeMap<>();
-
-        timestampedClosures.put(
-                ZonedDateTime.parse(delineatedRow[0]).toLocalDateTime(),
-                UUID.fromString(delineatedRow[1])
-        );
-
-        return timestampedClosures;
-    }
-
-    public static void appendToCsv(File file, String header, String row) {
-        try {
-            final PrintWriter writer = new PrintWriter(new FileWriter(file, true));
-
-            if (file.length() == 0) {
-                writer.println(header);
-            }
-
-            writer.println(row);
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.exit(1);
-        }
-    }
-
     public static void appendToFile(File file, String row) {
         try {
             final PrintWriter writer = new PrintWriter(new FileWriter(file, true));

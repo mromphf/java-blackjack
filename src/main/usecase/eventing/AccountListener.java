97d99f66b753e81b80f4dbbf417a6310282820b7
Mark Romphf <markromphf@gmail.com>
2022-03-07 21:00:20 -0600

    Kill the onAccountBalanceUpdated() method.

--- src/main/usecase/eventing/AccountListener.java
+++ src/main/usecase/eventing/AccountListener.java
@@ -8,6 +8,5 @@ public interface AccountListener {
     default void onAccountCreated(Account account) {}
     default void onAccountDeleted(Account account) {}
     default void onAccountSelected(Account account) {}
-    default void onAccountBalanceUpdated(Account account) {}
     default void onAccountsLoaded(Collection<Account> accounts) {}
 }

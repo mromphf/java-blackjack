b3cbbb6400ffbb7e84fae145c58c16a704c29300
Mark Romphf <markromphf@gmail.com>
2022-03-08 18:33:14 -0600

    Clean up the Card class.

--- src/main/domain/Card.java
+++ src/main/domain/Card.java
@@ -2,30 +2,29 @@ package main.domain;
 
 import java.util.HashMap;
 import java.util.Objects;
-import java.util.Random;
 
 public class Card {
-    private final int faceValue;
+    private final int value;
     private final Suit suit;
 
     public Card(int value, Suit suit) throws IllegalArgumentException {
         if (value > 13 || value < 1) {
             throw new IllegalArgumentException("Card value must be greater than 0 and less than 14.");
         }
-        this.faceValue = value;
+        this.value = value;
         this.suit = suit;
     }
 
     public boolean isAce() {
-        return faceValue == 1;
+        return value == 1;
     }
 
     public int getBlackjackValue() {
-        return Math.min(10, faceValue);
+        return Math.min(10, value);
     }
 
     public int getFaceValue() {
-        return faceValue;
+        return value;
     }
 
     public Suit getSuit() {
@@ -38,14 +37,7 @@ public class Card {
             put(11, "Jack");
             put(12, "Queen");
             put(13, "King");
-        }}.getOrDefault(faceValue, String.valueOf(faceValue));
-    }
-
-    public static Card random() {
-        Random r = new Random();
-        int v = r.nextInt(13) + 1;
-        int s = r.nextInt(4);
-        return new Card(v, Suit.values()[s]);
+        }}.getOrDefault(value, String.valueOf(value));
     }
 
     @Override
@@ -53,13 +45,13 @@ public class Card {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Card card = (Card) o;
-        return faceValue == card.faceValue &&
+        return value == card.value &&
                 suit == card.suit;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(faceValue, suit);
+        return Objects.hash(value, suit);
     }
 
     @Override

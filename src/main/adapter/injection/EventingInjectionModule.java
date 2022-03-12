e15df633a32b493061de04281cdba71c248abfdf
Mark Romphf <markromphf@gmail.com>
2022-03-12 12:26:42 -0600

    Dynamic injection for the Transaction Listeners.

--- src/main/adapter/injection/EventingInjectionModule.java
+++ src/main/adapter/injection/EventingInjectionModule.java
@@ -4,9 +4,14 @@ import com.google.inject.AbstractModule;
 import com.google.inject.Injector;
 import com.google.inject.Singleton;
 import com.google.inject.TypeLiteral;
+import main.adapter.log.GameLogger;
+import main.adapter.storage.AccountStorage;
+import main.usecase.AccountCache;
 import main.usecase.LayoutManager;
+import main.usecase.TransactionCache;
 import main.usecase.eventing.AlertListener;
 import main.usecase.eventing.EventNetwork;
+import main.usecase.eventing.TransactionListener;
 
 import java.util.Collection;
 import java.util.LinkedList;
@@ -31,6 +36,15 @@ public class EventingInjectionModule extends AbstractModule {
                 .annotatedWith(named("alertListeners"))
                 .toInstance(new LinkedList<AlertListener>() {{
                     add(fxmlInjector.getInstance(LayoutManager.class));
+                }});
+
+        bind(new TypeLiteral<Collection<TransactionListener>>() {})
+                .annotatedWith(named("transactionListeners"))
+                .toInstance(new LinkedList<TransactionListener>() {{
+                    add(baseInjector.getInstance(AccountCache.class));
+                    add(baseInjector.getInstance(AccountStorage.class));
+                    add(baseInjector.getInstance(GameLogger.class));
+                    add(baseInjector.getInstance(TransactionCache.class));
                 }});
     }
 }

23dca22031cefe9fa39ec2a0dc14439a8aadccc7
Mark Romphf <markromphf@gmail.com>
2022-03-12 11:38:44 -0600

    Manage the AccountCache with a Stack instead of a Map.

--- src/main/usecase/AccountCache.java
+++ src/main/usecase/AccountCache.java
@@ -8,53 +8,53 @@ import main.usecase.eventing.AccountListener;
 import main.usecase.eventing.EventConnection;
 import main.usecase.eventing.TransactionListener;
 
-import java.time.LocalDateTime;
 import java.util.Collection;
+import java.util.EmptyStackException;
 import java.util.Optional;
-import java.util.SortedMap;
-
-import static java.time.LocalDateTime.now;
+import java.util.Stack;
 
 public class AccountCache extends EventConnection implements AccountListener, TransactionListener {
 
-    private final SortedMap<LocalDateTime, Account> selections;
+    private final Stack<Account> selections;
 
     @Inject
-    public AccountCache(@Named("accountMap") SortedMap<LocalDateTime, Account> selections) {
+    public AccountCache(@Named("accountStack") Stack<Account> selections) {
         this.selections = selections;
     }
 
     public Optional<Account> getCurrentlySelectedAccount() {
-        if (selections.isEmpty()) {
+        if (selections.empty()) {
             return Optional.empty();
         } else {
-            return Optional.of(selections.get(selections.lastKey()));
+            return Optional.of(selections.peek());
         }
     }
 
     @Override
     public void onTransactionIssued(Transaction transaction) {
-        final Account currentState = selections.get(selections.lastKey());
-        final Account updatedState = currentState.updateBalance(transaction);
-
-        selections.put(now(), updatedState);
+        try {
+            selections.add(selections.pop().updateBalance(transaction));
+        } catch (EmptyStackException ex) {
+            ex.printStackTrace();
+        }
     }
 
     @Override
     public void onTransactionSeriesIssued(Collection<Transaction> transactions) {
-        final Account currentState = selections.get(selections.lastKey());
-        final Account updatedState = currentState.updateBalance(transactions);
-
-        selections.put(now(), updatedState);
+        try {
+            selections.add(selections.pop().updateBalance(transactions));
+        } catch (EmptyStackException ex) {
+            ex.printStackTrace();
+        }
     }
 
     @Override
     public void onAccountCreated(Account account) {
-        selections.put(now(), account);
+        selections.add(account);
     }
 
     @Override
     public void onAccountSelected(Account account) {
-        selections.put(now(), account);
+        selections.add(account);
     }
 }

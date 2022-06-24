5237ee5dc00da0df341dfa731550784cd78e36cd
Mark Romphf <markromphf@gmail.com>
2022-06-23 22:02:26 -0600

    New constant and DSL function.

--- src/main/domain/Deck.java
+++ src/main/domain/Deck.java
@@ -2,4 +2,9 @@ package main.domain;
 
 import java.util.Stack;
 
-public class Deck extends Stack<Card> {}
+public class Deck extends Stack<Card> {
+
+    public Card drawCard() {
+        return pop();
+    }
+}

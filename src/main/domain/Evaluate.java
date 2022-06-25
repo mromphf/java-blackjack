7db0b4a47d52655f48b656b95d5b4ea2227c1336
Mark Romphf <markromphf@gmail.com>
2022-06-24 20:16:25 -0600

    Inline transaction function calls.

--- src/main/domain/Evaluate.java
+++ src/main/domain/Evaluate.java
@@ -1,10 +1,8 @@
 package main.domain;
 
-import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Optional;
-import java.util.UUID;
 import java.util.function.Function;
 
 import static java.util.Optional.empty;
@@ -27,16 +25,16 @@ public class Evaluate {
     }
 
     private static Function<Snapshot, Optional<Transaction>> insuranceTransactions() {
-       return (snapshot) -> {
+        return (snapshot) -> {
             final Collection<Action> actionsTaken = snapshot.getActionsTaken();
             final boolean insurancePurchased = actionsTaken.size() == 1 && actionsTaken.contains(BUY_INSURANCE);
-            final LocalDateTime timestamp = snapshot.getTimestamp();
-            final UUID accountKey = snapshot.getAccountKey();
-            final String description = BUY_INSURANCE.name();
-            final int bet = snapshot.getNegativeBet();
 
-           if (insurancePurchased) {
-                return Optional.of(transaction(timestamp, accountKey, description, bet));
+            if (insurancePurchased) {
+                return Optional.of(transaction(
+                        snapshot.getTimestamp(),
+                        snapshot.getAccountKey(),
+                        BUY_INSURANCE.name(),
+                        snapshot.getNegativeBet()));
             } else {
                 return empty();
             }
@@ -45,15 +43,12 @@ public class Evaluate {
 
     private static Function<Snapshot, Optional<Transaction>> doubleDownTransactions() {
         return (snapshot) -> {
-            final Collection<Action> actionsTaken = snapshot.getActionsTaken();
-
-            if (actionsTaken.stream().filter(a -> a.equals(DOUBLE)).count() == 1) {
-                final LocalDateTime timestamp = snapshot.getTimestamp();
-                final UUID accountKey = snapshot.getAccountKey();
-                final String description = DOUBLE.name();
-                final int bet = snapshot.getNegativeBet();
-
-                return Optional.of(transaction(timestamp, accountKey, description, bet));
+            if (snapshot.getActionsTaken().stream().filter(a -> a.equals(DOUBLE)).count() == 1) {
+                return Optional.of(transaction(
+                        snapshot.getTimestamp(),
+                        snapshot.getAccountKey(),
+                        DOUBLE.name(),
+                        snapshot.getNegativeBet()));
             } else {
                 return empty();
             }
@@ -63,12 +58,11 @@ public class Evaluate {
     private static Function<Snapshot, Optional<Transaction>> outcomeTransactions() {
         return (snapshot) -> {
             if (outcomeIsResolved.test(snapshot)) {
-                final LocalDateTime timestamp = snapshot.getTimestamp();
-                final UUID accountKey = snapshot.getAccountKey();
-                final String description = snapshot.getOutcome().name();
-                final int payout = settleBet(snapshot);
-
-                return Optional.of(transaction(timestamp, accountKey, description, payout));
+                return Optional.of(transaction(
+                        snapshot.getTimestamp(),
+                        snapshot.getAccountKey(),
+                        snapshot.getOutcome().name(),
+                        settleBet(snapshot)));
             } else {
                 return empty();
             }
@@ -78,21 +72,17 @@ public class Evaluate {
     private static Function<Snapshot, Optional<Transaction>> splitTransactions() {
         return (snapshot) -> {
             final Collection<Action> actionsTaken = snapshot.getActionsTaken();
-            final LocalDateTime timestamp = snapshot.getTimestamp();
-            final UUID accountKey = snapshot.getAccountKey();
-            final String description = SPLIT.name();
-            final int bet = snapshot.getNegativeBet();
 
             final boolean chargeForSplit = (actionsTaken.stream().anyMatch(a -> a.equals(SPLIT)) &&
                     actionsTaken.stream().noneMatch(a ->
-                            a.equals(HIT) ||
-                            a.equals(DOUBLE) ||
-                            a.equals(STAND)));
-
-            final Transaction transaction = transaction(timestamp, accountKey, description, bet);
+                            a.equals(HIT) || a.equals(DOUBLE) || a.equals(STAND)));
 
             if (chargeForSplit) {
-                return Optional.of(transaction);
+                return Optional.of(transaction(
+                        snapshot.getTimestamp(),
+                        snapshot.getAccountKey(),
+                        SPLIT.name(),
+                        snapshot.getNegativeBet()));
             } else {
                 return empty();
             }

21a66f9038f45d72f24e2570600e396f9e0da579
Mark Romphf <markromphf@gmail.com>
2022-06-25 10:14:43 -0600

    Add isFaceUp boolean to Card.

--- src/main/domain/model/Card.java
+++ src/main/domain/model/Card.java
@@ -3,6 +3,7 @@ package main.domain.model;
 import java.util.Objects;
 import java.util.UUID;
 
+import static java.lang.Boolean.*;
 import static java.lang.Math.min;
 import static java.util.Objects.hash;
 import static java.util.UUID.randomUUID;
@@ -12,17 +13,31 @@ public class Card {
 
     private static final int HIGHEST_POSSIBLE_VALUE = 10;
 
+    private final Boolean isFaceUp;
     private final UUID key = randomUUID();
     private final Rank rank;
     private final Suit suit;
 
-    private Card(Rank rank, Suit suit) {
+    private Card(Rank rank, Suit suit, Boolean isFaceUp) {
         this.rank = rank;
         this.suit = suit;
+        this.isFaceUp = isFaceUp;
     }
 
     public static Card card(Rank rank, Suit suit) {
-        return new Card(rank, suit);
+        return new Card(rank, suit, TRUE);
+    }
+
+    public static Card card(Rank rank, Suit suit, Boolean isFaceUp) {
+        return new Card(rank, suit, isFaceUp);
+    }
+
+    public Card faceDown() {
+        return new Card(rank, suit, FALSE);
+    }
+
+    public Boolean isFaceUp() {
+        return isFaceUp;
     }
 
     public boolean isAce() {
@@ -45,10 +60,6 @@ public class Card {
         return this.suit == suit;
     }
 
-    public String name() {
-        return rank.NAME;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -64,6 +75,6 @@ public class Card {
 
     @Override
     public String toString() {
-        return String.format("%s of %s", name(), suit);
+        return String.format("%s of %s", rank.NAME, suit);
     }
 }

3f6914ac2867c90d48fec2a3c698464149f8873a
Mark Romphf <markromphf@gmail.com>
2022-06-26 09:41:54 -0600

    Delete the Transactor and give its work to the TransactionService.

--- src/main/usecase/TransactionService.java
+++ src/main/usecase/TransactionService.java
@@ -2,25 +2,34 @@ package main.usecase;
 
 import com.google.inject.Inject;
 import com.google.inject.name.Named;
+import main.domain.model.Snapshot;
 import main.domain.model.Transaction;
 import main.usecase.eventing.EventConnection;
+import main.usecase.eventing.SnapshotListener;
 import main.usecase.eventing.TransactionListener;
 
 import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static java.util.Collections.emptyList;
 import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.toList;
+import static main.adapter.injection.Bindings.EVALUATORS;
 import static main.adapter.injection.Bindings.TRANSACTION_MAP;
 
-public class TransactionService extends EventConnection implements TransactionListener {
+public class TransactionService extends EventConnection implements TransactionListener, SnapshotListener {
 
+    private final Collection<Function<Snapshot, Optional<Transaction>>> evaluationFunctions;
     public final Map<UUID, Collection<Transaction>> transactionMap;
 
     @Inject
-    public TransactionService(@Named(TRANSACTION_MAP) Map<UUID, Collection<Transaction>> transactionMap) {
+    public TransactionService(
+            @Named(EVALUATORS) Collection<Function<Snapshot, Optional<Transaction>>> evaluators,
+            @Named(TRANSACTION_MAP) Map<UUID, Collection<Transaction>> transactionMap) {
         this.transactionMap = transactionMap;
+        this.evaluationFunctions = evaluators;
     }
 
     public List<Transaction> getTransactionsByKey(UUID accountKey) {
@@ -37,13 +46,6 @@ public class TransactionService extends EventConnection implements TransactionLi
     }
 
     @Override
-    public void onTransactionSeriesIssued(Collection<Transaction> transactions) {
-        transactions.stream()
-                .collect(groupingBy(Transaction::getAccountKey))
-                .forEach(this::mapToCache);
-    }
-
-    @Override
     public void onTransactionsLoaded(Collection<Transaction> transactions) {
         transactions.stream()
                 .collect(groupingBy(Transaction::getAccountKey))
@@ -55,6 +57,23 @@ public class TransactionService extends EventConnection implements TransactionLi
             transactionMap.get(key).addAll(transactions);
         } else {
             transactionMap.put(key, transactions);
+        }
+    }
+
+    @Override
+    public void onGameUpdate(Snapshot snapshot) {
+        final Collection<Transaction> workingTransactions = evaluationFunctions.stream()
+                .map(f -> f.apply(snapshot))
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .collect(Collectors.toList());
+
+        workingTransactions.stream()
+                .collect(groupingBy(Transaction::getAccountKey))
+                .forEach(this::mapToCache);
+
+        if (workingTransactions.size() > 0) {
+            eventNetwork.onTransactionSeriesIssued(workingTransactions);
         }
     }
 }

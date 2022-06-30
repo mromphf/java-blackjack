8a9c81c8873970a35d68cded6a8f0cac9cd2e3fc
Mark Romphf <markromphf@gmail.com>
2022-06-30 16:16:45 -0600

    Add a Selection Service interface.

---
+++ src/main/usecase/SelectionService.java
@@ -0,0 +1,9 @@
+package main.usecase;
+
+import main.domain.model.Account;
+import java.util.Optional;
+
+public interface SelectionService {
+
+    Optional<Account> getCurrentlySelectedAccount();
+}

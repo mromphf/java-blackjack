ff36dabfdc8c71e049bf8471d9777fc35926232f
Mark Romphf <markromphf@gmail.com>
2022-07-01 11:46:35 -0600

    Add BetAssessment function.

---
+++ src/main/domain/function/BetAssessment.java
@@ -0,0 +1,38 @@
+package main.domain.function;
+
+import main.domain.Assessment;
+import main.domain.model.Action;
+import main.domain.model.Snapshot;
+import main.domain.model.Transaction;
+
+import java.util.Optional;
+
+import static java.util.Optional.empty;
+import static main.domain.model.Action.BET;
+import static main.domain.model.Transaction.transaction;
+import static main.domain.predicate.RoundPredicate.startOfRound;
+
+public class BetAssessment implements Assessment {
+
+    public static BetAssessment betAssessment() {
+        return new BetAssessment();
+    }
+
+    @Override
+    public Action action() {
+        return BET;
+    }
+
+    @Override
+    public Optional<Transaction> apply(Snapshot snapshot) {
+        if (startOfRound.test(snapshot)) {
+            return Optional.of(transaction(
+                    snapshot.getTimestamp(),
+                    snapshot.getAccountKey(),
+                    "BET",
+                    snapshot.getNegativeBet()));
+        } else {
+            return empty();
+        }
+    }
+}

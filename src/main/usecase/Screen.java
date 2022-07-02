217954de7540e32de159272466e6ba5c940c03e6
Mark Romphf <markromphf@gmail.com>
2022-07-02 11:00:33 -0600

    Decorate Screen enum with resource paths.

--- src/main/usecase/Screen.java
+++ src/main/usecase/Screen.java
@@ -1,10 +1,30 @@
 package main.usecase;
 
+import java.util.Collection;
+
+import static java.util.stream.Collectors.toSet;
+import static java.util.stream.Stream.of;
+
 public enum Screen {
-    BACK,
-    BET,
-    GAME,
-    HOME,
-    HISTORY,
-    REGISTRATION
+    BACK("NO PATH"),
+    BET("../ui/bet/BetView.fxml"),
+    GAME("../ui/blackjack/BlackjackView.fxml"),
+    HOME("../ui/home/HomeView.fxml"),
+    HISTORY("../ui/history/HistoryView.fxml"),
+    REGISTRATION("../ui/registration/RegistrationView.fxml"),
+    ;
+
+    private final String path;
+
+    Screen(String path) {
+        this.path = path;
+    }
+
+    public String path() {
+        return path;
+    }
+
+    public static Collection<Screen> screensWithPaths() {
+        return of(BET, GAME, HOME, HISTORY, REGISTRATION).collect(toSet());
+    }
 }

2a6ac8cd3e6a96f8352a61d4eb494dabf2a12e33
Mark Romphf <markromphf@gmail.com>
2022-07-03 17:22:49 -0600

    Include 70x70 png symbol files.

--- src/main/adapter/graphics/Symbol.java
+++ src/main/adapter/graphics/Symbol.java
@@ -3,10 +3,10 @@ package main.adapter.graphics;
 public enum Symbol {
     BLUE_CARD("card_blue"),
     RED_CARD("card_red"),
-    SYMBOL_CLUBS("sym_clubs"),
-    SYMBOL_DIAMONDS("sym_diamonds"),
-    SYMBOL_HEARTS("sym_hearts"),
-    SYMBOL_SPADES("sym_spades"),
+    SYMBOL_CLUBS("sym_clubs_small"),
+    SYMBOL_DIAMONDS("sym_diamonds_small"),
+    SYMBOL_HEARTS("sym_hearts_small"),
+    SYMBOL_SPADES("sym_spades_small"),
     ;
 
     public final String VALUE;

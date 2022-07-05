8b802a0899c618f3cfdf7f304a045dfd347633d7
Mark Romphf <markromphf@gmail.com>
2022-07-04 20:32:31 -0600

    Redo the reel animation with vectors instead of rectangles.

--- src/main/adapter/graphics/MovingImage.java
+++ src/main/adapter/graphics/MovingImage.java
@@ -9,42 +9,32 @@ import static main.adapter.ui.Direction.RIGHT;
 
 public class MovingImage {
 
+    private final static int TOP = 0;
     private final static int BOUNDARY_LEFTMOST = 880;
-    private final static int IMAGE_SIZE = 40;
 
     private final Image img;
-    private final double velocity;
-    private final double width;
-    private final double height;
-    private final double y;
+    private final int velocity;
+    private final Vector vector;
 
     private Direction direction;
-    private double x;
 
     public MovingImage(final Image img,
                        final Vector v,
-                       final float velocity,
+                       final int velocity,
                        Direction direction) {
         this.img = img;
         this.velocity = velocity;
-        this.width = v.dimension;
-        this.height = v.dimension;
-        this.x = v.position;
+        this.vector = v;
         this.direction = direction;
-        this.y = 0;
     }
 
     public void move() {
-        if (direction == LEFT) {
-            x -= velocity;
-        } else {
-            x += velocity;
-        }
+        vector.move(direction, velocity);
 
-        if ((direction == LEFT) && ((x + width) < 0)) {
-            x = BOUNDARY_LEFTMOST;
-        } else if (x - width >= (BOUNDARY_LEFTMOST - IMAGE_SIZE)) {
-            x = (IMAGE_SIZE * (-1));
+        if ((direction == LEFT) && ((vector.position + vector.dimension) < 0)) {
+            vector.relocate(BOUNDARY_LEFTMOST);
+        } else if ((vector.position - vector.dimension) >= (BOUNDARY_LEFTMOST - vector.dimension)) {
+            vector.relocate((vector.dimension * (-1)));
         }
     }
 
@@ -53,6 +43,6 @@ public class MovingImage {
     }
 
     public void draw(GraphicsContext graphics) {
-        graphics.drawImage(img, x, y, width, height);
+        graphics.drawImage(img, vector.position, TOP, vector.dimension, vector.dimension);
     }
 }
--- src/main/adapter/graphics/Vector.java
+++ src/main/adapter/graphics/Vector.java
@@ -1,9 +1,13 @@
 package main.adapter.graphics;
 
+import main.adapter.ui.Direction;
+
+import static main.adapter.ui.Direction.LEFT;
+
 public class Vector {
 
-    public final int position;
     public final int dimension;
+    public int position;
 
     private Vector(int position, int dimension) {
         this.position = position;
@@ -12,5 +16,17 @@ public class Vector {
 
     public static Vector vector(int position, int dimension) {
         return new Vector(position, dimension);
+    }
+
+    public void move(Direction direction, int velocity) {
+        if (direction == LEFT) {
+            position = (position - velocity);
+        } else {
+            position = (position + velocity);
+        }
+    }
+
+    public void relocate(int position) {
+        this.position = position;
     }
 }

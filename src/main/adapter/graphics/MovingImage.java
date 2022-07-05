f39fdfaa9b5041a64fb245202755990784fa78a0
Mark Romphf <markromphf@gmail.com>
2022-07-04 19:28:05 -0600

    Assign integer names.

--- src/main/adapter/graphics/MovingImage.java
+++ src/main/adapter/graphics/MovingImage.java
@@ -10,6 +10,9 @@ import static main.adapter.ui.Direction.RIGHT;
 
 public class MovingImage {
 
+    private final static int BOUNDARY_LEFTMOST = 880;
+    private final static int IMAGE_SIZE = 40;
+
     private final Image img;
     private final double velocity;
     private final double width;
@@ -36,10 +39,10 @@ public class MovingImage {
             x += velocity;
         }
 
-        if ((direction == LEFT) && x + width < 0) {
-            x = 880;
-        } else if (x - width >= 840){
-            x = -40;
+        if ((direction == LEFT) && ((x + width) < 0)) {
+            x = BOUNDARY_LEFTMOST;
+        } else if (x - width >= (BOUNDARY_LEFTMOST - IMAGE_SIZE)) {
+            x = (IMAGE_SIZE * (-1));
         }
     }
 

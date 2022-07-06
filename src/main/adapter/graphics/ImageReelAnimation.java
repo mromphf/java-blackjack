16c19c85cef6e4fb75f9fbfd60b831116c039775
Mark Romphf <markromphf@gmail.com>
2022-07-06 17:41:38 -0600

    Take out constants and a method.

--- src/main/adapter/graphics/ImageReelAnimation.java
+++ src/main/adapter/graphics/ImageReelAnimation.java
@@ -12,10 +12,11 @@ import static main.adapter.graphics.Vector.vector;
 
 public class ImageReelAnimation extends AnimationTimer {
 
+    private final static int CANVAS_WIDTH = 840;
     private final static int IMG_SIZE = 40;
     private final static int REEL_SPEED = 1;
     private final static int REEL_LENGTH = 23;
-    private final static int TOP = 0;
+    private final static int ORIGIN = 0;
 
     private final GraphicsContext graphics;
     private final Collection<Moving<Image>> imageReel = new LinkedList<>();
@@ -24,7 +25,6 @@ public class ImageReelAnimation extends AnimationTimer {
         this.graphics = graphics;
 
         for (int i = 0, position = 0, imageIndex = 0; i < REEL_LENGTH; i++, position += IMG_SIZE) {
-
             final Moving<Image> moving = new Moving<>(images[imageIndex], vector(position, IMG_SIZE), REEL_SPEED, direction);
 
             imageReel.add(moving);
@@ -39,17 +39,21 @@ public class ImageReelAnimation extends AnimationTimer {
 
     @Override
     public void handle(long now) {
-        graphics.clearRect(0, 0, 840, 40);
+        clearCanvas();
         for (Moving<Image> img : imageReel) {
             img.move();
-            graphics.drawImage(img.data(), img.vector().position, TOP, img.vector().dimension, img.vector().dimension);
+            graphics.drawImage(img.data(), img.vector().position, ORIGIN, img.vector().dimension, img.vector().dimension);
         }
     }
 
     public void switchDirection() {
-        graphics.clearRect(0, 0, 840, 40);
+        clearCanvas();
         for (Moving<Image> moving : imageReel) {
             moving.switchDirection();
         }
+    }
+
+    public void clearCanvas() {
+        graphics.clearRect(ORIGIN, ORIGIN, CANVAS_WIDTH, IMG_SIZE);
     }
 }

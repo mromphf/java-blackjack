d4c26257f5b2073a510dcb73aa3e6ca51687282f
Mark Romphf <markromphf@gmail.com>
2022-07-15 14:36:45 -0600

    Reorganizing predicates.

--- src/main/domain/predicate/LowOrderPredicate.java
+++ src/main/domain/predicate/LowOrderPredicate.java
@@ -18,32 +18,47 @@ public class LowOrderPredicate {
     public static final Predicate<Snapshot> outcomeIsResolved = snapshot ->
             snapshot.getOutcome() != null && snapshot.getOutcome() != UNRESOLVED;
 
-    public static final Predicate<Snapshot> atLeastOneCardDrawn = snapshot -> snapshot.getPlayerHand().size() > 2;
+    public static final Predicate<Snapshot> atLeastOneCardDrawn = snapshot ->
+            snapshot.getPlayerHand().size() > 2;
 
-    public static final Predicate<Snapshot> handsRemainToBePlayed = snapshot -> !snapshot.getHandsToPlay().isEmpty();
+    public static final Predicate<Snapshot> handsRemainToBePlayed = snapshot ->
+            !snapshot.getHandsToPlay().isEmpty();
 
-    public static final Predicate<Snapshot> noActionsTaken = snapshot -> snapshot.getActionsTaken().isEmpty();
+    public static final Predicate<Snapshot> noActionsTaken = snapshot ->
+            snapshot.getActionsTaken().isEmpty();
 
-    public static final Predicate<Snapshot> handsRemainToBeSettled = snapshot -> !snapshot.getHandsToSettle().isEmpty();
+    public static final Predicate<Snapshot> handsRemainToBeSettled = snapshot ->
+            !snapshot.getHandsToSettle().isEmpty();
 
-    public static final Predicate<Snapshot> playerHasBusted = snapshot -> isBust(snapshot.getPlayerHand());
+    public static final Predicate<Snapshot> playerHasBusted = snapshot ->
+            isBust(snapshot.getPlayerHand());
+
+    public static final Predicate<Snapshot> dealerHasAce = snapshot ->
+            snapshot.getDealerHand().stream().filter(Card::isFaceUp).limit(1).allMatch(Card::isAce);
+
+    public static final Predicate<Snapshot> playerPurchasedInsurance = snapshot ->
+            snapshot.getActionsTaken()
+                    .stream()
+                    .anyMatch(a -> a == Action.BUY_INSURANCE);
 
     public static final Predicate<Snapshot> turnEnded = snapshot ->
             snapshot.getActionsTaken().stream().anyMatch(Action::turnEnded);
+
+    public static final Predicate<Snapshot> startOfRound = (snapshot) -> (
+            outcomeIsUnresolved.and(noActionsTaken).test(snapshot));
 
     public static final Predicate<Snapshot> readyToSettleNextHand = snapshot ->
             outcomeIsResolved.and(handsRemainToBeSettled).test(snapshot);
 
     public static final Predicate<Snapshot> isInsuranceAvailable = snapshot ->
-            noActionsTaken.test(snapshot) &&
-                    snapshot.getDealerHand().stream().filter(Card::isFaceUp).limit(1).allMatch(Card::isAce);
+            not(playerPurchasedInsurance).and(dealerHasAce).and(noActionsTaken).test(snapshot);
 
     public static final Predicate<Snapshot> readyToPlayNextHand = snapshot -> (
-            outcomeIsUnresolved.and(handsRemainToBePlayed).and((playerHasBusted.or(turnEnded))).test(snapshot));
+            outcomeIsUnresolved.and(handsRemainToBePlayed).and(playerHasBusted.or(turnEnded)).test(snapshot));
 
     public static final Predicate<Snapshot> isGameInProgress = snapshot -> (
             !canSplit(snapshot.getPlayerHand()) &&
-                    outcomeIsUnresolved.and(isInsuranceAvailable.negate().and(readyToPlayNextHand.negate())).test(snapshot));
+                    outcomeIsUnresolved.and(not(isInsuranceAvailable).and(not(readyToPlayNextHand))).test(snapshot));
 
     public static final Predicate<Snapshot> isSplitAvailable = (snapshot) -> (
             canSplit(snapshot.getPlayerHand()) &&
@@ -51,7 +66,4 @@ public class LowOrderPredicate {
 
     public static final Predicate<Snapshot> allBetsSettled = (snapshot) -> (
             outcomeIsResolved.and(not(handsRemainToBePlayed)).and(not(handsRemainToBeSettled))).test(snapshot);
-
-    public static final Predicate<Snapshot> startOfRound = (snapshot) -> (
-            outcomeIsUnresolved.and(noActionsTaken).test(snapshot));
 }

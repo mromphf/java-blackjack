b8108b70162b7adeeb4da47ac89ed3af0b623b8c
Mark Romphf <markromphf@gmail.com>
2022-07-17 11:51:16 -0600

    Additions to the postgres 'create schema' script.

--- src/sql/create-schema.sql
+++ src/sql/create-schema.sql
@@ -1,4 +1,19 @@
+-- Buyer beware,
+-- This script assumes a database named 'blackjack' exists
 
+
+CREATE TABLE IF NOT EXISTS blackjack.accounts (
+    key UUID PRIMARY KEY,
+    timestamp TIMESTAMP WITH TIME ZONE NOT NULL,
+    name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS blackjack.transactions (
+    accountKey UUID PRIMARY KEY,
+    timestamp TIMESTAMP WITH TIME ZONE NOT NULL,
+    description VARCHAR(20) NOT NULL,
+    amount INTEGER
+);
 
 CREATE TABLE IF NOT EXISTS blackjack.account_closures (
     key UUID PRIMARY KEY,

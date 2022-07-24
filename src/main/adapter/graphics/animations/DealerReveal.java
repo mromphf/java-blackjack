f85c03da6f6ff984c6d949b49c19874ed5a61aa0
Mark Romphf <markromphf@gmail.com>
2022-07-24 09:27:41 -0600

    Class for the dealer reveal animation.

---
+++ src/main/adapter/graphics/animations/DealerReveal.java
@@ -0,0 +1,11 @@
+package main.adapter.graphics.animations;
+
+import javafx.animation.AnimationTimer;
+
+public class DealerReveal extends AnimationTimer {
+
+    @Override
+    public void handle(long now) {
+
+    }
+}

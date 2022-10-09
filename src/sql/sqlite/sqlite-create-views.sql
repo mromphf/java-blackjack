92e3befe792f8cf6a5434e4ced70d26765437a11
Mark Romphf <markromphf@gmail.com>
2022-10-09 11:22:23 -0600

    Create a second account balance view that includes closed accounts.

--- src/sql/sqlite/sqlite-create-views.sql
+++ src/sql/sqlite/sqlite-create-views.sql
@@ -1,5 +1,6 @@
-DROP VIEW IF EXISTS blackjack.account_balances;
-DROP VIEW IF EXISTS blackjack.active_transactions;
+DROP VIEW IF EXISTS account_balances;
+DROP VIEW IF EXISTS account_balances_inc_closed;
+DROP VIEW IF EXISTS active_transactions;
 
 CREATE VIEW active_transactions AS
      SELECT accountkey, description, amount, t.timestamp
@@ -8,12 +9,26 @@ CREATE VIEW active_transactions AS
         ON t.accountKey = a.key
     WHERE a.key NOT IN (SELECT key FROM account_closures);
 
+
 CREATE VIEW account_balances AS
-    SELECT a.key, a.timestamp, a.name,
-        SUM(t.amount) AS "balance"
-      FROM accounts a
-      JOIN transactions t
-        ON a.key = t.accountkey
-     WHERE a.key NOT IN (SELECT key FROM account_closures)
-  GROUP BY a.key, a.name, a.timestamp
-  ORDER BY a.name, "balance", "transactions";
+SELECT a.key, a.timestamp, a.name,
+       SUM(t.amount) AS balance,
+       COUNT(t.accountKey) AS transactions
+FROM accounts a
+         JOIN transactions t
+              ON a.key = t.accountkey
+WHERE a.key NOT IN (SELECT key FROM account_closures)
+GROUP BY a.key, a.name, a.timestamp
+ORDER BY balance DESC;
+
+
+CREATE VIEW account_balances_inc_closed AS
+SELECT a.key, a.timestamp, a.name,
+       SUM(t.amount) AS balance,
+       COUNT(t.accountKey) AS transactions
+FROM accounts a
+         JOIN transactions t
+              ON a.key = t.accountkey
+WHERE a.key NOT IN (SELECT key FROM account_closures)
+GROUP BY a.key, a.name, a.timestamp
+ORDER BY balance DESC;

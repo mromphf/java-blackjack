6108500b43543d95e4fdada79447377d0d449217
Mark Romphf <markromphf@gmail.com>
2022-10-09 18:18:23 -0600

    Rearrange the order of Card inheritance.

--- src/main/domain/model/AnonymousCard.java
+++ src/main/domain/model/AnonymousCard.java
@@ -1,27 +1,72 @@
 package main.domain.model;
 
 import static java.util.Objects.hash;
+import static main.domain.model.Rank.ACE;
 
-public class AnonymousCard extends Card {
+public class AnonymousCard {
 
-    private AnonymousCard(Suit suit, Rank rank) {
-        super(rank, suit, (true));
+    protected final boolean isFaceUp;
+    protected final Rank rank;
+    protected final Suit suit;
+
+    protected AnonymousCard(Suit suit, Rank rank) {
+        this.rank = rank;
+        this.suit = suit;
+        this.isFaceUp = false;
+    }
+
+    protected AnonymousCard(Suit suit, Rank rank, boolean isFaceUp) {
+        this.suit = suit;
+        this.rank = rank;
+        this.isFaceUp = isFaceUp;
     }
 
     public static AnonymousCard anonymousCard(Suit suit, Rank rank) {
         return new AnonymousCard(suit, rank);
     }
 
+    public static AnonymousCard anonymousCard(Suit suit, Rank rank, boolean isFaceUp) {
+        return new AnonymousCard(suit, rank, isFaceUp);
+    }
+
     public String shortName() {
         return suit.name().toLowerCase() + rank().ORDINAL;
+    }
+
+    public AnonymousCard faceDown() {
+        return new AnonymousCard(suit, rank, (false));
+    }
+
+    public boolean isFaceUp() {
+        return isFaceUp;
+    }
+
+    public boolean isAce() {
+        return rank == ACE;
+    }
+
+    public int blackjackValue() {
+        return rank.BLACKJACK_VALUE;
+    }
+
+    public Rank rank() {
+        return rank;
+    }
+
+    public boolean is(Suit suit) {
+        return this.suit == suit;
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Card card = (Card) o;
-        return rank == card.rank() && suit == card.suit();
+
+        AnonymousCard that = (AnonymousCard) o;
+
+        if (isFaceUp != that.isFaceUp) return false;
+        if (rank != that.rank) return false;
+        return suit == that.suit;
     }
 
     @Override
--- src/main/domain/model/Card.java
+++ src/main/domain/model/Card.java
@@ -1,32 +1,22 @@
 package main.domain.model;
 
-import java.util.Objects;
 import java.util.UUID;
 
 import static java.util.Objects.hash;
 import static java.util.UUID.randomUUID;
-import static main.domain.model.AnonymousCard.anonymousCard;
-import static main.domain.model.Rank.ACE;
 
-public class Card {
+public class Card extends AnonymousCard {
 
-    private final boolean isFaceUp;
     protected final UUID key;
-    protected final Rank rank;
-    protected final Suit suit;
 
     private Card(UUID key, Rank rank, Suit suit, boolean isFaceUp) {
+        super(suit, rank, isFaceUp);
         this.key = key;
-        this.rank = rank;
-        this.suit = suit;
-        this.isFaceUp = isFaceUp;
     }
 
     protected Card(Rank rank, Suit suit, boolean isFaceUp) {
+        super(suit, rank, isFaceUp);
         this.key = randomUUID();
-        this.rank = rank;
-        this.suit = suit;
-        this.isFaceUp = isFaceUp;
     }
 
     public static Card card(Rank rank, Suit suit) {
@@ -41,30 +31,6 @@ public class Card {
         return new Card(key, rank, suit, (false));
     }
 
-    public boolean isFaceUp() {
-        return isFaceUp;
-    }
-
-    public boolean isAce() {
-        return rank == ACE;
-    }
-
-    public int blackjackValue() {
-        return rank.BLACKJACK_VALUE;
-    }
-
-    public Rank rank() {
-        return rank;
-    }
-
-    public Suit suit() {
-        return suit;
-    }
-
-    public boolean is(Suit suit) {
-        return this.suit == suit;
-    }
-
     public AnonymousCard anonymize() {
         return anonymousCard(suit, rank);
     }
@@ -73,8 +39,9 @@ public class Card {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
         Card card = (Card) o;
-        return Objects.equals(key, card.key) && rank == card.rank && suit == card.suit;
+        return key.equals(card.key);
     }
 
     @Override

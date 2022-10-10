9b61752d6bde7f4d6333b6f9b577bea0b59f8dca
Mark Romphf <markromphf@gmail.com>
2022-10-10 10:43:16 -0600

    Sqlite seed data script for ranks and suits.

---
+++ src/sql/sqlite/sqlite-seed-data.sql
@@ -0,0 +1,10 @@
+INSERT INTO suits
+VALUES ('Clubs'),
+       ('Diamonds'),
+       ('Hearts'),
+       ('Spades');
+
+INSERT INTO ranks
+VALUES (1, 'Ace', 1), (2, 'Two', 2), (3, 'Three', 3), (4, 'Four', 4),
+       (5, 'Five', 5), (6, 'Six', 6), (7, 'Seven', 7), (8, 'Eight', 8), (9, 'Nine', 9),
+       (10, 'Ten', 10), (11, 'Jack', 10), (12, 'Queen', 10), (13, 'King', 10);

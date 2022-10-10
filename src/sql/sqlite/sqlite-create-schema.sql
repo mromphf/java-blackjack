c3f15218a7368fb65d0856ec1ba026c82e0099d5
Mark Romphf <markromphf@gmail.com>
2022-10-10 10:48:35 -0600

    Merge schema and seed data scripts.

--- src/sql/sqlite/sqlite-create-schema.sql
+++ src/sql/sqlite/sqlite-create-schema.sql
@@ -38,3 +38,14 @@ CREATE TABLE IF NOT EXISTS ranks
     blackjackValue INT CHECK (
         blackjackValue > 0 AND blackjackValue < 11)
 );
+
+INSERT INTO suits
+VALUES ('Clubs'),
+       ('Diamonds'),
+       ('Hearts'),
+       ('Spades');
+
+INSERT INTO ranks
+VALUES (1, 'Ace', 1), (2, 'Two', 2), (3, 'Three', 3), (4, 'Four', 4),
+       (5, 'Five', 5), (6, 'Six', 6), (7, 'Seven', 7), (8, 'Eight', 8), (9, 'Nine', 9),
+       (10, 'Ten', 10), (11, 'Jack', 10), (12, 'Queen', 10), (13, 'King', 10);
